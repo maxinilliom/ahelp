3de04c5f7b4201975468d4a06237167a980a7ad2
maxruahar <maximillionrh@gmail.com>
2018-04-09 11:54:46 +0000

    melee.js -> help.js

---
+++ guides/maxGuides/help.js
@@ -0,0 +1,26 @@
+exports.data = {
+  "help": {
+    "title": "Comprehensive list of all valid skills",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+  "search": {
+    "title": "All skills matching your search",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  }
+};
