c8aa59b8afbb10bbe0bdcc7f1141075db90dc520
maxruahar <maximillionrh@gmail.com>
2018-06-06 13:44:06 +0000

    update welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,19 +7,13 @@ module.exports = async (client, member) => {
 	const welcomeChannel = client.channels.get("382700760317558786");
 	const replyChannel = client.channels.get('435588740346413057');
 	const logChannel = client.channels.get('407919969712603145');
-	const wMsg = `\n\nIf you have any of the following, please respond to this message letting us know and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss or Master of All titles\n• 1,693/1,841 achievements or 18,252/19,840 RuneScore\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with more than 50 followers\n\nWe also have a friend chat in game: 'Saint Cannon'\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
+	const wMsg = `\n\nIf you have any of the following, please let us know and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss or Master of All titles\n• 1,693/1,841 achievements or 18,252/19,840 RuneScore\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with more than 50 followers\n\nWe also have a friend chat in game: 'Saint Cannon'\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
 	const maxUsers = "5";
 
-	if (client.guilds.get('382696689812766720').memberCount == 2000 && client.twok == "true") {
-		welcomeChannel.send(`Congratulations, ${member.toString()}, you are our 2000th member! Contact any Council Member to receive your prize!`);
-		welcomeChannel.send(`Thank you to everyone who has joined and supported us on this incredible journey to support and benefit the RuneScape community in every way possible. We appreciate every one of you and all that you do for the server, Achievement Help would be nothing without **you**! ❤️`);
-		client.twok = "false";
-	}
-
-	const collEnd = (coll, arr) => {
+/*	const collEnd = (coll, arr) => {
 		coll.stop();
 	}
-
+*/
 	const welcome = async () => {
 		if (client.newUsers.size == "0") return;
 		let userIDs = client.newUsers.map(u => u.id);
@@ -38,7 +32,7 @@ module.exports = async (client, member) => {
 		let userList = userArrOld.join(", ");
 		const msg = `Welcome in ${userList}!${wMsg}`;
 		welcomeChannel.send(msg);
-		let output = [];
+/*		let output = [];
 		const msgs = [];
 
 		client.msgColl = new Discord.MessageCollector(welcomeChannel, m => userIDsOld.includes(m.author.id), {maxMatches: userArrOld.length});
@@ -52,6 +46,7 @@ module.exports = async (client, member) => {
 		client.msgColl.on("end", collected => {
 			replyChannel.send(`${output.join("\n")}\nAll users' responses logged or timed out for ${userList}`);
 		});
+*/
 	}
 
 	if (client.welcomeTimer._called !== false) client.welcomeTimer = setTimeout(welcome, 5 * 60 * 1000);
