dd99414514c02983b848873e50296247d0adfa2e
maxruahar <maximillionrh@gmail.com>
2018-06-07 17:07:53 +0000

    rewrite for challenge 2

--- commands/five.js
+++ commands/five.js
@@ -1,8 +1,18 @@
 exports.run = (client, message, args, level) => {
 	if (args[0] == "five") return message.channel.send(`Successful attempts so far: **${client.five}**`);
 	const x = max => Math.floor(Math.random() * Math.floor(max));
-	if (x(1000) > 997 || message.author.id == '97928972305707008') message.channel.send('<:grats:383025290927931394>');
-	client.five++;
+	if (client.fiveLast !== message.author.id && x(1000) > 995) {
+		client.fiveLast = message.author.id;
+		client.five++;
+		if (!args[0]) {
+			message.reply('Your letter was received but we\'re sad to see that there was no message.');
+		} else if (args.join(" ").length <= 32) {
+			message.reply('Success! Your message has been received. Now, where did I put it?');
+			message.guild.members.get('212672527405678592').setNickname(`${args.join(' ')}`);
+		} else {
+			message.reply('I\'m sorry, your message is too long. Try again!');
+		}
+	}
 };
 
 exports.conf = {
