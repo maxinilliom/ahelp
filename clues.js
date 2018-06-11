7ab509f30571c82727ff76eddac2180cc0c2768d
maxruahar <maximillionrh@gmail.com>
2018-06-11 12:04:13 +0000

    store clues for scav and event commands

---
+++ clues.js
@@ -0,0 +1,12 @@
+module.exports = {
+        '204106577374871554': ["Kharidian Desert.", "Leap of faith.", "The room below me could really use a rework."],
+        '226801823808356353': ["The winds and the waves are always on the side of the ablest navigators.", "You must path Southwest to Eastnorth and onwards to southeast and once again north.", "I guide the strong who drink and feast in honour of the one quest wonder"],
+        '245184851504791552': ["I think your map is upside down! This isn't Neitznot.", "She said go deeper but im all out of Salve eels", "Land ho! We've arrived at.... it looks like a new skill? No, a minigame? I dont know."],
+        '295278315671912450': ["As they attack, these troops must be set in motion.", "End of the line for this fowl that started with breadcrumbs.", "A very vibrant place to be."],
+        '226913163717640194': ["I've found the library of the gods.", "I feel like this dwelling might float away.", "Fires are usually dangerous to boats, but this one protects them."],
+        '294617234033213450': ["Blue birds nest on me.", "You may walk, teleport, or ride in a horse drawn cart to get to me.", "You may make green runes here."],
+        '145074943300730880': ["It's quite windy up here, at least there's someone to talk to.", "That's a weird name for a bird.", "My arm is quite tired after that adventure."],
+        '140219862248062976': ["Looks like a long drop from here!", "I can still hear that little man shouting", "Ah, so this is where all the bad kids go!"],
+        '212672527405678592': ["I’ve ruined lives and crushed dreams, But the allure is too much it seems.", "It’s against the rules for you to play fair, I always win but you don’t care.", "It will take you 800 hours to get to me!"],
+        '97928972305707008': ["Surrounded by glass, yet unrestricted in movement.", "A dragon can be found three floors below me.", "Touch this cape and you'll be transmogrified!"]
+};
