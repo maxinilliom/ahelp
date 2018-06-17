4ee1a80403ed901ff57520f4e19ec5fffdabe701
Maximillion <maximillionrh@gmail.com>
2018-06-17 02:11:58 +0100

    add div level responses

---
+++ guides/maxGuides/divination.js
@@ -0,0 +1,264 @@
+exports.data = {
+	"help": {
+		"title": "Comprehensive list of all valid commands",
+		"author": {
+			"name": "Max Cape Info",
+			"icon_url": "https://i.imgur.com/6c6q2iC.png"
+			},
+		"description": "{{output}}",
+		"footer": {
+			"icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+			"text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+		}
+	},
+	"search": {
+		"title": "All commands matching your search",
+		"author": {
+			"name": "Max Cape Info",
+			"icon_url": "https://i.imgur.com/6c6q2iC.png"
+		},
+		"description": "{{output}}",
+		"footer": {
+			"icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+			"text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+		}
+	},
+	"1 - 10": {
+    "title": "Divination Training Levels 1-10",
+    "description": "At level 1 Divination players should begin training at the [Pale Wisp Colony](http://runescape.wikia.com/wiki/Pale_wisp). With level 1-10 Divination players can obtain around 5,000 experience an hour from training at pale wisps using additional bought energy, requiring approximately 725 pale energy  per hour, or 4,000 experience an hour if not buying additional energy. \n\n As only 1,154 experience is required to reach level 10 Divination, players with no previous experience of the skill should achieve level 10 in approximately 19 minutes, using around 225 pale energy if using bought energy. ",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Pale_wisps_.28levels_1.E2.80.9310.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/3/3e/Pale_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207090710"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "10 - 20": {
+    "title": "Divination Training Levels 10-20",
+    "description": "At level 10, players can move on to training at the [Flickering Wisp Colony](http://runescape.wikia.com/wiki/Flickering_wisp), which is found east of the Falador lodestone. Around 10,000 experience an hour can be gained by training at flickering wisps if the player purchases additional energy, or 8,000 experience an hour without buying additional energy. This is the first location at which players may encounter enriched versions of a colony's wisps. ",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Flickering_wisps_.28levels_10.E2.80.9320.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/3/3e/Pale_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207090710"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "20 - 30": {
+    "title": "Divination Training Levels 20-30",
+    "description": "With at least level 20 Divination players can move to the [Bright Wisp Colony](http://runescape.wikia.com/wiki/Bright_wisp), which is found east of Varrock, west of the Digsite. When buying additional energy, players can gain around 13,000 experience an hour from training at bright wisps, or 11,000 experience an hour if not buying additional energy.",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Bright_wisps_.28levels_20.E2.80.9330.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/1/1d/Bright_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207090804"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "30 - 40": {
+    "title": "Divination Training Levels 30-40",
+    "description": "From level 30 players can train south of Seers' Village, just east of the Sorcerer's Tower, at the [Glowing Wisp Colony](http://runescape.wikia.com/wiki/Glowing_wisp)to gain approximately 18,000 experience an hour, if using additional purchased energy, or 15,000 experience an hour if not using additional energy.",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Bright_wisps_.28levels_20.E2.80.9330.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/d/dd/Glowing_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207090826"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "40 - 50": {
+    "title": "Divination Training Levels 40-50",
+    "description": "Players with level 40-50 Divination can train at the [Sparkling Wisp Colony](http://runescape.wikia.com/wiki/Sparkling_wisp) to gain around 23,000 experience an hour without purchasing additional energy, or 28,000 experience an hour if using additional energy. This colony is located just south-west of the Fremennik Slayer Dungeon and may be reached using the nearby fairy ring (code ajr)..",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Sparkling_wisps_.28levels_40.E2.80.9350.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/0/0c/Sparkling_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207090847"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "50 - 60": {
+    "title": "Divination Training Levels 60-70",
+    "description": "At level 60 Divination players should start training at the [Vibrant Wisp Colony](http://runescape.wikia.com/wiki/Vibrant_wisp), just north of Mobilising Armies (A spirit tree is located right next to the colony). Around 40,000 experience an hour may be gained by players who are not using additional purchased energy. Around 53,000 experience an hour may be gained if using additional energy. \n\n If [The Dig Site](http://runescape.wikia.com/wiki/The_Dig_Site) quest has not been completed continue this method until level 70-75",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Vibrant_wisps_.28levels_60.E2.80.9365.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/1/10/Vibrant_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207091004"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "65 - 75": {
+    "title": "Divination Training Levels 65-75",
+    "description": "With at least 65 Divination players who have completed [The Dig Site](http://runescape.wikia.com/wiki/The_Dig_Site) quest can gain Divination experience from [The Empty Throne Room](http://runescape.wikia.com/wiki/The_Empty_Throne_Room). Players can gain a total of 800,000 Divination experience from the Empty Throne Room: this is enough to level from 65-75 Divination. It is advised that the player uses the method to train from level 65-75 as it gives the greatest improvement in experience rates over alternative training methods available when used at these levels and is free. Training at the throne room gives around 66,000 Divination experience an hour (at level 99 Divination).\n\nTo gain Divination experience the player must take senntisten crystals from the crystal storage bin, located near the room's entrance; transmute them into red, yellow, green and blue crystals; and place them into the nearby bins of the corresponding colour. One of the coloured bins is empowered, with this changing approximately every minute. Transmuting senntisten crystals to the empowered colour gives 10 times the regular experience so players should prioritise transmuting to this colour. Experience from depositing into the empowered bin does not grant extra experience.",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#The_Empty_Throne_Room_.28levels_65.E2.80.9375.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/9/95/Zaros%27s_Throne_Room.png/revision/latest/scale-to-width-down/638?cb=20171006224121"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "70 - 80": {
+    "title": "Divination Training Levels 70-80",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Elder_wisps_or_lustrous_wisps_.28levels_75.E2.80.9380.29",
+    "description": "​",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/9/93/Lustrous_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207091015"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Lustrous wisps",
+        "value": "The [Lustrous Wisp Colony](http://runescape.wikia.com/wiki/Lustrous_wisp) is found just east of the Slayer Tower. Without purchasing additional energy, players may earn around 54,000 experience an hour from training using lustrous wisps. Players who have purchased additional energy can gain around 66,000 experience an hour."
+      },
+      {
+        "name": "Elder wisps (Level 75-80)",
+        "value": "Players who have completed the [Fate of the Gods](http://runescape.wikia.com/wiki/Fate_of_the_Gods) quest can train using [elder wisps](http://runescape.wikia.com/wiki/Elder_wisp), found within the [Elder Halls](http://runescape.wikia.com/wiki/Elder_Halls), from level 75 to 80 Divination. At this location enriched wisps spawn in the corridors, rather than near the crater which is located at the centre of the area."
+      }
+      ]
+  },
+  "80 - 85": {
+    "title": "Divination Training Levels 80-85",
+    "description": "With level 80-85 Divination players can train at the [Brilliant Wisp Colony](http://runescape.wikia.com/wiki/Brilliant_wisp), just east of the Mage Training Arena. Players who are training without purchasing additional energy can gain around 60,000 experience an hour from training at brilliant wisps, while 73,000 experience an hour can be gained using extra energy.\n\nPlayers who have at least one level 99 skill or who have completed all quests can build a brilliant wisp colony portal within the garden of the Max guild in Prifddinas to teleport directly to the brilliant wisp colony. Alternatively, if fairy rings are unlocked, the fairy ring bip can be used to teleport directly north of the colony at the entrance of the Polypore Dungeon.",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Brilliant_wisps_.28levels_80.E2.80.9385.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/1/17/Brilliant_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207092352"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "85 - 90": {
+    "title": "Divination Training Levels 85-90",
+    "description": "From level 85–90 Divination players should train on Dragontooth Island at the [Radiant Wisp Colony](http://runescape.wikia.com/wiki/Radiant_wisp). Around 65,000 experience an hour can be gained by players who are training at radiant wisps without using additional bought energy. When using additional energy players can gain up to 79,000 experience an hour.\n\n Players who have at least one level 99 skill or who have completed all quests can build a radiant wisp colony portal within the garden of the Max guild in Prifddinas to teleport directly to the radiant wisp colony. Players who have the hoardstalker ring or the Dungeoneering Cape can also use that to teleport to the wisp colony.",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Radiant_wisps_.28levels_85.E2.80.9390.29",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/f/f8/Radiant_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207091053"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "90 - 95": {
+    "title": "Divination Training Levels 90-95",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Luminous_wisps_.28levels_90.E2.80.9395.29",
+    "description": "​",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/b/b2/Luminous_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207091105"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Luminous wisps",
+        "value": "At level 90 Divination players can train using luminous wisps to gain around 71,000 experience an hour without using bought energy or around 83,000 experience an hour using bought energy. The [Luminous Wisp Colony](http://runescape.wikia.com/wiki/Luminous_wisp) is located south of Sophanem, in the Kharidian Desert.\n\nPlayers who have at least one level 99 skill or who have completed all quests can build a luminous wisp colony portal within the garden of the Max guild in Prifddinas to teleport directly to the luminous wisp colony."
+      },
+      {
+        "name": "Positive and Negative energy",
+        "value": "Alternatively at level 90 Divination, players can visit [Goshima](http://runescape.wikia.com/wiki/Goshima), [Cyclosis](http://runescape.wikia.com/wiki/Cyclosis), or [The Islands That Once Were Turtles](http://runescape.wikia.com/wiki/The_Islands_That_Once_Were_Turtles) in [The Arc](http://runescape.wikia.com/wiki/The_Arc) and harvest positive or negative energy, with positive being found on Cyclosis and negative being found on Goshima. Both energies can be found together on The Islands That Once Were Turtles. These energies are only gathered, not deposited, and bringing one type of energy to a spring of the other type creates ancestral energy which can otherwise only be found on Uncharted Isles.\n\nBoth of these energy springs offer the same experience per hour, which is around 68,000 experience per hour normally, but can be higher with the addition of boosting equipment mentioned at the top of this page. This also earns around 660 chimes per hour, which is important for those trying to progress in The Arc."
+      }
+      ]
+  },
+  "95 - 99 +": {
+    "title": "Divination Training Levels 95-99+",
+    "url": "http://runescape.wikia.com/wiki/Divination_training#Levels_95-99.2B:_ancestral_energy_.28levels_95.E2.80.9399.29",
+    "description": "​",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Divination"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/f/f5/Incandescent_wisp.png/revision/latest/scale-to-width-down/200?cb=20180207091117"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Ancestral energy",
+        "value": "At level 95 Divination, players who have access to [Uncharted Isles](http://runescape.wikia.com/wiki/Uncharted_Isles) in [The Arc](http://runescape.wikia.com/wiki/The_Arc) can train on ancestral energy. Players must find dead moai on the island which are leaking ancestral energy and collect it. The energy is collected but no memories are produced or deposited, meaning all the experience comes directly from the gathering. With this in mind, no energy must be purchased or gathered to increase the experience per hour. This method gives approximately 107,000 experience per hour.\n\nThe experience from harvesting ancestral energy can further be bolstered by completing contracts given by [Sojobo (contracts)](http://runescape.wikia.com/wiki/The_Tengu) on Waiko or via a contract contact on an uncharted isle. Divination contracts for ancestral energy give up to about 35,000 experience when handed in, though they tend to average about 24,000 experience each."
+      },
+      {
+        "name": "Incandescent Wisp",
+        "value": "At level 95 Divination can siphon from the [Incandescent Wisp Colony](http://runescape.wikia.com/wiki/Incandescent_wisp). This is located just south of the Poison Waste, south-west of Castle Wars. Players may obtain approximately 97,000 experience an hour from training on incandescent wisps using additional bought energy with level 99 Divination, requiring approximately 3,200 incandescent energy per hour. Around 81,500 experience can be gained per hour with level 99 Divination if the player is not purchasing additional energy.\n\nPlayers who have at least one level 99 skill or who have completed all quests can build an incandescent wisp colony portal within the garden of the Max guild in Prifddinas to teleport directly to the incandescent wisp colony."
+      },
+      {
+        "name": " Weaving Incandescent Energy",
+        "value": "At level 95 Divination players can weave [Cursed Energy](http://runescape.wikia.com/wiki/Cursed_energy) into [Incandescent Energy](http://runescape.wikia.com/wiki/Incandescent_energy) for approximately 210,000 experience per hour. Around 140,000 cursed energy is used per hour, to create 210,000 incandescent energy. Cursed energy cannot be traded through the Grand Exchange and must be bought from other players. Although this method is usually not profitable, the high experience rates and low intensity can make up for this."
+      }
+      ]
+  }
+};
