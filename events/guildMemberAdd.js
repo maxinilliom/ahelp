34221d0233a218a068f93e3d959b407868085856
maxruahar <maximillionrh@gmail.com>
2018-06-21 00:51:51 +0000

    update title requirements

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,7 +7,7 @@ module.exports = async (client, member) => {
 	const welcomeChannel = client.channels.get("382700760317558786");
 	const replyChannel = client.channels.get('435588740346413057');
 	const logChannel = client.channels.get('407919969712603145');
-	const rs = '• 1,697/1,845 achievements or 18,294/19,885 RuneScore';
+	const rs = '• 1,740/1,892 achievements or 18,863/20,275 RuneScore';
 	const wMsg = `\n\nIf you have any of the following, please let us know and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss or Master of All titles\n${rs}\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with more than 50 followers\n\nWe also have a friend chat in game: 'Saint Cannon'\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
 	const maxUsers = "5";
 
