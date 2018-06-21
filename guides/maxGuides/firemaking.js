1a5c37681890aef320bc446be6b34f9c548b56ab
Maximillion <maximillionrh@gmail.com>
2018-06-21 12:04:35 +0100

    initial commit of firemaking embeds

---
+++ guides/maxGuides/firemaking.js
@@ -0,0 +1,34 @@
+exports.data = {
+	"help": {
+    "title": "Comprehensive list of all valid commands",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+  "search": {
+    "title": "All commands matching your search",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+	"1 - ": ,
+  "alternate methods": ,
+  "pet - ": ,
+  "quests": ,
+  "distractions and diversions": ,
+  "daily challenge": ,
+  "permanent boosts": ,
+  "temporary boosts": 
+};
