93feb0e0a7e7a7026856223f4d15857f2884b990
maxruahar <maximillionrh@gmail.com>
2018-07-14 13:47:43 +0100

    initial commit of dungeoneering embeds

---
+++ guides/maxGuides/dungeoneering.js
@@ -0,0 +1,251 @@
+exports.data = {
+	"help": {
+    "title": "Comprehensive list of all valid commands",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+  "search": {
+    "title": "All commands matching your search",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+  "1 - 58": {
+    "title": "Dungeoneering Level 1-58",
+      "description": "Upon first entering Daemonheim, you will only have access to minimal dungeon set ups, namely complexity 1 and floor 1. For low Dungeoneering levels, it is recommended to solo. \n\n • Talk to the Dungeoneering tutor by Daemonheim's entrance, he will give you a Ring of kinship. \n\n • Form a party by right clicking your Ring of kinship and selecting ``Open Party Interface`` and click ``Form Party``. \n\n • To start a dungeon after forming a party, click on the entrances that can be found on the left or right sides as soon as you enter the castle. \n\n • You will start on floor 1, complexity 1. You will be provided with some equipment. You may only bind one item at level 1 Dungeoneering. It is recommended to bind the highest level 2-handed weapon available. \n\n • You will want to do small or medium Frozen floors (floors 1-11) followed by small or medium Abandoned 1 floors (floors 12-17). These can be done solo or in a group. Prestige your progress every time you complete every floor available at your dungeoneering level. When you reach level 35 you can do Furnished Floors (floors 18-29) These you want to do large and it is highly reccomended to do them with a team.",
+      "url": "https://runescape.wikia.com/wiki/Pay-to-play_Dungeoneering_training#Optimising_floors",
+      "color": 12269891,
+      "footer": {
+        "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+        "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+      },
+      "thumbnail": {
+        "url": ""
+      },
+      "author": {
+        "name": "Max Cape Info",
+        "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+      "fields": [
+      {
+        "name": "Completion",
+        "value": "On completion you will receive an experience penalty and bonus, the penalty for the low complexity and the bonus for your first run. The complexity will then be increased for the next dungeon. The bonus experience will be modified so that you'll get Dungeoneering level 2. Repeat these floors on the highest complexity you can."
+      },
+      {
+        "name": "Prestiging",
+        "value": "Knowing when to reset prestige is key to getting greater amounts of Dungeoneering experience for completing higher-level floors. For example, you will get less experience from floor 1 than from floor 10; but once you have successfully raided floors 1-10 and you do not have the Dungeoneering level to access the next floor, you should reset your progress."
+      }
+    ]
+  },
+  "58 - 70": {
+    "title": "Dungeoneering Level 58-70",
+    "description": "At this stage you will want to be rushing all of your Frozen and Abandoned 1 floors on complexity level 1. For the Furnished floors to optimise your experience you will want to do floors 18-24 small and then 25-29 large in a team. You will want to do your available Abandoned 2 floors in a team as well set to large. Prestige once youve completed all floors you can and repeat this process until you have reached level 70 Dungeoneering. \n\n • The official world to train dungeoneering is world 77. As dungeoneering can be trained in every world, players who want to train solo or have already found other players to train with are recommended to choose a world in which they have the best connection. \n\n For AHelp members interested in finding partners or groups for dungeoneering training you can get the role `Notify Dungeoneering Team` in the channel `<# 465998663710146579>`.",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "70 - 94": {
+    "title": "Dungeoneering Levels 70-94",
+    "description": "From here you want to be rushing through all of the lower floors (1-29) in Complexity one and doing the remaining Abandoned 2 and Occult floors in larges with a group. Prestiging as before making sure you have completed all floors unlocked at your current dungeoneering level.",
+    "url": "https://runescape.wikia.com/wiki/Pay-to-play_Dungeoneering_training#Optimising_floors",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+   
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "94 - 120": {
+    "title": "Dungeoneering Levels 94-120",
+    "url": "https://runescape.wikia.com/wiki/Pay-to-play_Dungeoneering_training#Optimising_floors",
+    "description": "From level 95 to 120 and beyond you will want to rush all floors including Abandoned 2 (floor 1-35) and only complete Occult and Warped floors in large Complexity 6 with a team. (floors 36-60) You will prestige your progress each time you complete all floors and repeat until you have achieved your level/experience goal. ",
+    "url": "https://runescape.wikia.com/wiki/Pay-to-play_Dungeoneering_training#Optimising_floors",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "suggestions": {
+    "title": "Suggestions",
+    "description": "• Go to the deepest floor as you can with the Dungeoneering level you have and reset prestige when you have done all the floors you can. \n\n •It is advised to get the highest tier of pickaxes and hatchets you can possibly obtain (even if you do not have the required level to use it, since they automatically become the highest tier your skill can use until you can use their true tier) to add to your Dungeoneering tool belt as early as possible. This will enable you to obtain resources faster and will also last you for other floors. For example, somebody with 76 Mining can add a primal pickaxe to their tool belt, as the primal pickaxe will act as the highest tier pickaxe you can use - in this case, tier 8. \n\n • ```Party with people like you```: It is always a good idea to group up with people near your combat level and with similar skill levels. This ensures that the monsters in the dungeon and skill requirements will not be too hard for any one person. Also, you should be able to agree on how you progress through a dungeon, killing everything, skilling all the way or rushing.",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/1/15/Dungeoneering.png/revision/latest?cb=20120223124111"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+      "fields":[  
+         {  
+            "name":"​",
+            "value":" •  ```Maximise your mods```: Set the complexity and difficulty to the highest possible, kill as much as you can and open all doors you have access to. Complexity 6 has no experience penalty, complexity 5 has a 30% experience penalty, and each complexity thereafter has another 5% penalty, all the way down to 50% penalty for complexity 1. Difficulty won't give a penalty unless there is a large range in player levels, but can give a big bonus for sometimes little work. Additionally, using a lower complexity and difficulty actually carries a much larger penalty than the 30%-50% modifier that you see. The base floor experience and base prestige experience are affected by several unseen factors, two of which, either directly or indirectly are complexity level and difficulty ratio. "
+         },
+         {  
+            "name":"​",
+            "value":"The only time you would want to set a lower complexity is when you are trying to move through the low floors (typically 1-30 if you have over 40 floors unlocked) at maximum speed in order to maximise your experience rate by spending more time doing deep large floors. Killing things will increase your level mod, but that can be time consuming, and it is up to you to decide whether it's worth an extra 10%. Bonus rooms should always be opened, as they offer up to 13% extra experience for almost nothing."
+         },
+         {  
+            "name":"​",
+            "value":" • ```Turn off guide mode```: Always set guide mode to off as this gives a negative bonus of -2 to -12% (possibly more) which can seriously affect your experience and token gain. Guide mode cannot be turned off in complexity 1 through 4."
+         },
+         {  
+            "name":"​",
+            "value":" • ```Survival```: Try to avoid needless deaths. Dying can have big experience penalties (between 4% and 70%, you can die up to 15 times, any deaths after 15 don't count) so it is ideal to avoid dying whenever you can. However, with that said, teams of intermediate and more experienced dungeoneerers will generally grow quite angry with people hoarding food and avoiding dangerous rooms/running away in the middle of combat in order to avoid death at all costs. These people are known as \"survival prods\" and are generally an annoyance and slow down floor times greatly. In order to obtain the best possible experience rates per hour, it is a better strategy to take one or two deaths per floor and save the 5 minutes it would take to return to base, gather food materials, cook food etc, and then resume combat."
+         },
+         {  
+            "name":"​",
+            "value":" • ```Be a team player```: Join your team when they call out 'gd', as this is a guardian door and they'll want your help. If you see a key, pick it up so that everyone has access to it and open any doors you see. Don't go into a room if they say 'de', its a dead end you don't have to explore. If a team member asks you to make them some armour, ask them to get you the materials, and vice versa. If someone needs food and they're out, do some good and feed them some of yours. All this can speed up a run through the dungeon. However, in certain situations, devoting too much time to skilling can slow down the floor significantly."
+         },
+         {  
+            "name":"​",
+            "value":"\n\n • ```Raise other skill levels```: Dungeoneering is unique in many ways, one of which is its use of all other skills. You may find that by levelling your other skills dungeons will progress faster, as you have access to more resources and methods of obtaining them, giving you better armour, food, and familiars. It is also advised to keep your skills fairly even; at 80 woodcutting, you'll be getting corpsethorn and entgallow trees, but if your Fletching and/or Firemaking isn't up to the levels for these trees, you will usually be forced to buy them from the smuggler, or ask your teammates for some help. Also be aware that your teammates levels are incorporated into the dungeon, and if their levels are higher than yours, you may find yourself surrounded by resources you can't use, let alone gather."
+         }
+      ]
+  },
+  "alternate methods": {
+    "title": "Alternative Methods of Training",
+    "url": "",
+    "description": "Some users may wish to utilize methods outside of the ones displayed in this channel's guide. To aid in this, the following alternative methods are provided.",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/5/5c/Sinkhole_entrance.png/revision/latest/scale-to-width-down/250?cb=20130412213226"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "​Sinkholes",
+        "value": "The Sinkholes Distraction and Diversion is a short, five-player raid that can be completed twice daily. It allows players to receive Dungeoneering experience from reward lamps, in addition to tokens. "
+      },
+       {
+        "name": "Resource Dungeons",
+        "value": " It is also possible to get large amounts of experience (a total of 86,600 experience for members or 12,000 experience for non-members) for uncovering the Resource dungeons spread throughout RuneScape. As soon as you get to the required level, head to the dungeon. For lower levels (1-20), it is guaranteed that you will level up 1-5 levels per visit to a dungeon. "
+      },
+      {
+        "name": "Elite Dungeons​",
+        "value": "Elite dungeons are a set of dungeons that function similar to Raids. Rewards from these dungeons include Dungeoneering experience and tokens as well as a chance to receive loot that include weapons and armour upgrades that range from tier 82 to 92. There are two modes, story and normal mode, and players can choose to solo or join groups of up to three. Story mode reduces the damage enemies deal by 66%, as well as their lifepoints by 50%, but yields no rewards, except lore books, and only 10% of Dungeoneering tokens per miniboss/boss.  "
+      }
+      ]
+  },
+  "pet - gordie": {
+    "title": "Dungeoneering Pet - Gordie",
+    "description": "Gordie is the skilling pet obtainable from Dungeoneering. He is unlocked by inspecting the [Gordie Pet](http://runescape.wikia.com/wiki/Gordie_pet).",
+    "url": "http://runescape.wikia.com/wiki/Gordie",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://vignette.wikia.nocookie.net/runescape2/images/b/ba/Gordie_pet_detail.png/revision/latest?cb=20160823180842",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/b/ba/Gordie_pet_detail.png/revision/latest?cb=20160823180842"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Methods to Obtain",
+        "value": "It is unlocked by completing a dungeon floor. Gaining this pet is based on the amount of experience awarded at once; the more experience granted per floor, the greater the chance of gaining this pet. The chance is not affected by experience boosters. All enemies from the lowest tier up to the main bosses in Elite Dungeons also give a small chance to receive the Gordie pet. "
+      },
+      {
+        "name": "Best Method",
+        "value": "• The highest chance of unlocking Gordie is through doing solo medium floors at 200M Dungeoneering XP. "
+      },
+      {
+        "name": "Methods which do not Work",
+        "value": "• Unlocking resource dungeons does not give a chance of unlocking Gordie, nor does the exp given from the lamp reward from Sinkholes."
+      },
+      {
+        "name": "Formula",
+        "value": "Gaining this pet is based on the amount of experience received at once, the larger the experience drop the higher the chance of receiving the pet. However it is also divided amongst the players in your team. Therefore, it is advised to do solo medium floors for the highest experience per hour without dividing the pet chance among your teammates.\n\n In a purely mathematical format your chance (f) of receiving a pet on any particular action is:\n f = B*S / 1,250,000,000*P \n\n Where:\n*B* is the base experience per action\n*S* is the virtual skill level. (At 200 million experience, a flat bonus of 200 is applied to this value.) and P is the Party size."
+      }
+      ]
+  },
+  "quests": {
+    "title": "Quests which Grant Dungeoneering Experience",
+    "url": "http://runescape.wikia.com/wiki/Quests/Experience_rewards#Dungeoneering",
+    "description": "Currently, a total of 106,000 experience may be gained specifically to Dungeoneering throughout all of the RuneScape Quests. These rewards are automatically given and there is no choice involved.",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/8/8d/Quest_Icon_Crest.png/revision/latest?cb=20170206115901"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Complete List by RS Wiki",
+        "value": "The full list may be found [here](http://runescape.wikia.com/wiki/Quests/Experience_rewards#Dungeoneering)."
+      }
+      ]
+  },
+  "distractions and diversions": {
+    "title": "Embed missing"
+  },
+  "daily challenge": {
+    "title": "Daily Challenge",
+    "description": "Completing a daily challenge in Dungeoneering will give Dungeoneering experience. The amount depends only on a player's level (right after the daily reset), whether they are a member, and whether the task is extended. Based on data, the extended experience is always twice the normal experience (for members), and the non-member experience ratio depends on level.",
+    "url": "https://runescape.wikia.com/wiki/Challenge_System/Dungeoneering",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+   
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "permanent boosts": {
+    "title": "Embed missing"
+  },
+  "temporary boosts": {
+    "title": "Embed missing"
+  }
+};
