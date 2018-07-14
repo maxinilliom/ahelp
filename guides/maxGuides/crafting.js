31caf791613d19273aff3b1fa37d4d431e779f6e
maxruahar <maximillionrh@gmail.com>
2018-07-14 13:07:22 +0100

    initial commit of crafting embeds

---
+++ guides/maxGuides/crafting.js
@@ -0,0 +1,230 @@
+exports.data = {
+	"help": {
+    "title": "Comprehensive list of all valid commands",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+  "search": {
+    "title": "All commands matching your search",
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "description": "{{output}}",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    }
+  },
+  "1 - 54": {
+    "title": "Crafting Training Levels 1-54",
+    "description": "Players should start by visiting [Fritz the Glassblower](http://runescape.wikia.com/wiki/Fritz_the_Glassblower) on [Entrana](http://runescape.wikia.com/wiki/Entrana) who can teach the player how to make [Molten Glass](http://runescape.wikia.com/wiki/Glass). You will need to craft 3,400 molten glass to reach level 46, at which point you can use glassblowing to turn the molten glass into [Unpowered Orbs](http://runescape.wikia.com/wiki/Unpowered_orb).",
+    "url": "http://runescape.wikia.com/wiki/Pay-to-play_Crafting_training#Level_1_-_46.2B:_Glassblowing",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/f/f9/Molten_glass_detail.png/revision/latest/scale-to-width-down/100?cb=20111126045053"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "54 - 63": {
+    "title": "Crafting Training Levels 54-63",
+    "description": "At level 54 you can craft [Water Battlestaves](http://runescape.wikia.com/wiki/Water_battlestaff). You will need to craft 2,178 Water Battlestaves to reach level 63. The cost for this section can optionally be mitigated by using [High Level Alchemy](http://runescape.wikia.com/wiki/High_Level_Alchemy) on the Water Battlestaves you craft.",
+    "url": "http://runescape.wikia.com/wiki/Pay-to-play_Crafting_training#Level_54.2B",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/4/47/Water_battlestaff_detail.png/revision/latest/scale-to-width-down/130?cb=20130414202352"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "63 - 99 +": {
+    "title": "Crafting Training Levels 63-99+",
+    "description": "D'hide bodies/shields",
+    "url": "http://runescape.wikia.com/wiki/Pay-to-play_Crafting_training#Dragonhide_armour_-_fast_and_expensive",
+    "color": 13118778,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/1/19/Green_dragon_leather_detail.png/revision/latest/scale-to-width-down/100?cb=20111122081340"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "alternate methods": {
+    "title": "Alternative Methods of Training",
+    "description": "Harps, Citadel, Dungeoneering, Mention Dice AF Gem Cutting",
+    "url": "http://runescape.wikia.com/wiki/Pay-to-play_Crafting_training#Other_ways_to_train",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/2/23/Ruby_detail.png/revision/latest/scale-to-width-down/100?cb=20170917104546"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "pet - gemi": {
+    "title": "Crafting Pet - Gemi",
+    "description": "Gemi is the skilling pet obtainable from Crafting. He is unlocked by inspecting the [Gemi Pet](http://runescape.wikia.com/wiki/Gemi_pet).",
+    "url": "http://runescape.wikia.com/wiki/Gemi",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/b/be/Gemi_%28pet%29.png/revision/latest/scale-to-width-down/200?cb=20160902181109"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Methods to Obtain",
+        "value": "• Crafting Leathers\n• Crafting Battlestaves\n• Crafting Jewellery\n• Glassblowing\n• Using a Potter's Wheel\n• Using a Spinning Wheel\n• Crafting Gizmo Shells\n• Playing Harps\n• Crafting Tortle Shell Bowls\n"
+      },
+      {
+        "name": "Best Method",
+        "value": "• Black Dragonhide Shield"
+      },
+      {
+        "name": "Methods which do not Work",
+        "value": "• Using [Protean Hides](http://runescape.wikia.com/wiki/Protean_hide)."
+      },
+      {
+        "name": "Formula",
+        "value": "Gaining this pet is based on the amount of experience gained at once, not the number of experience drops.\n\nIn a purely mathematical format your chance (f) of receiving a pet on any particular action is:\nf = B *S / 1,000,000,000\n\nWhere:\n*B* is the base experience per action\n*S* is the virtual skill level. At 200 million experience, a flat bonus of 200 is applied to this value."
+      }
+      ]
+  },
+  "quests": {
+    "title": "Quests which Grant Crafting Experience",
+    "url": "http://runescape.wikia.com/wiki/Quests/Experience_rewards#Crafting",
+    "description": "Currently, a total of 554,899 experience may be gained specifically to crafting throughout all of the RuneScape Quests. These rewards are automatically given and there is no choice involved. It is possible to gain a further 60,000 experience by creating [Illuminated God Books](http://runescape.wikia.com/wiki/Illuminated_god_book) post [One Piercing Note](http://runescape.wikia.com/wiki/One_Piercing_Note).",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/8/8d/Quest_Icon_Crest.png/revision/latest?cb=20170206115901"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Complete List by RS Wiki",
+        "value": "The full list may be found [here](http://runescape.wikia.com/wiki/Quests/Experience_rewards#Crafting)."
+      }
+      ]
+  },
+  "distractions and diversions": {},
+  "daily challenge": {
+    "title": "Daily Challenge",
+    "url": "http://runescape.wikia.com/wiki/Challenge_System/Crafting",
+    "description": "Completing a daily challenge in Crafting will give Crafting experience, depending on the player's Crafting level.",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/d/d5/Daily_Challenge_icon.png/revision/latest?cb=20140711174355"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "​",
+        "value": "At level 98, 46,800 experience is awarded per day for making 70 [Decorated Cooking Urns](http://runescape.wikia.com/wiki/Decorated_cooking_urn). Players must hand in the urns to recieve a [Challenge Mystery Bag](http://runescape.wikia.com/wiki/Challenge_Mystery_Bag).\n\nDaily challenges may be extended for 50 [Vis Wax](http://runescape.wikia.com/wiki/Vis_wax) for double the reward.\n\nA full chart of the experience per level for the daily challenge may be found [here](http://runescape.wikia.com/wiki/Challenge_System/Crafting)."
+      }
+      ]
+  },
+  "permanent boosts": {
+    "title": "Permanent Boosts",
+    "url": "http://runescape.wikia.com/wiki/Bonus_experience_items#Crafting",
+    "description": "A permanent skill boost is anything caused by an item that permanently raises a player's level in one or more skills. It allows the player to do things above their current level.",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/5/55/Artisan%27s_outfit_equipped.png/revision/latest/scale-to-width-down/150?cb=20151120140418"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Crafting Cape",
+        "value": "• The [Crafting Cape's](http://runescape.wikia.com/wiki/Crafting_cape) perk removes the need for thread when crafting [leathers](http://runescape.wikia.com/wiki/Leather_(disambiguation)."
+      },
+      {
+        "name": "Artisan's Outfit",
+        "value": "• Wearing components of the [Artisan's Outfit](http://runescape.wikia.com/wiki/Artisan%27s_outfit) gives a percentage increase in the amount of Crafting experience gained. There are five pieces in the set: the headwear, chestpiece, legwear, gloves and boots. Components of the Artisan's outfit can be won from Treasure Hunter or obtained as a reward from [Stealing Creation](http://runescape.wikia.com/wiki/Stealing_Creation) for 100 reward points per piece. Each piece provides 1% additional agility experience when worn. If all pieces of the set are worn, an additional set bonus effect of 1% is given, for a total of 6%."
+      },
+      {
+        "name": "Dwarven Army Axe",
+        "value": "• Wearing the [Dwarven Army Axe](http://runescape.wikia.com/wiki/Dwarven_army_axe), or having it in your inventory gives 3 additional experience when crafting items using [Soft Leather](http://runescape.wikia.com/wiki/Leather) and [Soft Clay](http://runescape.wikia.com/wiki/Soft_clay)."
+      }
+      ]
+  },
+  "temporary boosts": {
+    "title": "Temporary Boosts",
+    "url": "http://runescape.wikia.com/wiki/Temporary_skill_boost",
+    "description": "A temporary skill boost is anything caused by an item, Summoning familiar, or action that temporarily raises a player's level in one or more skills. It allows the player to do things above their current level.",
+        "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/0/0a/Crafting_potion_detail.png/revision/latest/scale-to-width-down/50?cb=20140712190652"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "fields": [
+      {
+        "name": "Crafting Boosts",
+        "value": "[Crafting Potion](http://runescape.wikia.com/wiki/Crafting_potion)\n •When consumed, Crafting Potions temporarily raise the player's Crafting level by 3.\n\n[Spicy Stew](http://runescape.wikia.com/wiki/Spicy_stew)\n • Boost is applied to a random skill\n • Up to ±5 with 3 spice doses\n • Up to ±3 with 2 spice doses\n • Up to ±1 with 1 spice dose\n\n[God Banner](http://runescape.wikia.com/wiki/God_banner)\n• Skill boost: +2 levels\n• The boost can be used once per day and lasts for 30 minutes\n\n[Voice of Seren - Ithell](http://runescape.wikia.com/wiki/Voice_of_Seren#Known_effects)\n• 20% more base Crafting and Construction XP from training using the [harps](http://runescape.wikia.com/wiki/Harp).\n• 20% more base Crafting XP from crafting [crystal flasks](http://runescape.wikia.com/wiki/Crystal_flask).\n"
+      }
+      ]
+  }
+};
