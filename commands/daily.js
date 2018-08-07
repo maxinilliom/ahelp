a2ee2de8d89c90a826a4c0abe0e8300980b7dea2
maxruahar <maximillionrh@gmail.com>
2018-08-07 17:05:55 +0000

    what was I thinking

--- commands/daily.js
+++ commands/daily.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args, level) => {
 	if (message.channel.id !== '407919969712603145' && level < 2) return;
 
 	Object.getOwnPropertyNames(data).forEach(k => {
-		if (!data[k].title.includes("commands");) keyList.push(k);
+		if (!data[k].title.includes("commands")) keyList.push(k);
 	});
 	if (!args[0]) return message.channel.send(`Please specify a valid guide name.`);
 
