f7d0a9643b1c65aea31cc820fe4aaf62777a4de8
maxruahar <maximillionrh@gmail.com>
2018-09-05 10:04:11 +0000

    add fourth help embed

--- commands/truetrim.js
+++ commands/truetrim.js
@@ -62,14 +62,17 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 		let output = "";
 		let second = "";
 		let third = "";
+		let fourth = "";
 		const helpEmbed = data["help"];
 		keyList.forEach(k => {
-			if (output.length <= 2000) {
+			if (output.length <= 1950) {
 				output += `• ${data[k].title}\n`;
-			} else if (second.length <= 2000) {
+			} else if (second.length <= 1950) {
 				second += `• ${data[k].title}\n`;
-			} else {
+			} else if (third.length <= 1950) {
 				third += `• ${data[k].title}\n`;
+			} else if (fourth.length <= 1950) {
+				fourth += `• ${data[k].title}\n`;
 			}
 		});
 		helpEmbed.title = "Comprehensive list of all valid True Trimmed guides";
@@ -77,12 +80,12 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 		helpEmbed.description = output;
 		helpEmbed.color = color;
 		helpEmbed.timestamp = new Date();
-		message.channel.send("", {embed: helpEmbed});
+		await message.channel.send("", {embed: helpEmbed});
 
 		if (second.length > 0) {
 			helpEmbed.description = second;
 			helpEmbed.timestamp = new Date();
-			message.channel.send("", {embed: helpEmbed});
+			await message.channel.send("", {embed: helpEmbed});
 		}
 
 		if (third.length > 0) {
@@ -90,6 +93,13 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 			helpEmbed.timestamp = new Date();
 			await message.channel.send("", {embed: helpEmbed});
 		}
+
+		if (fourth.length > 0) {
+			helpEmbed.description = fourth;
+			helpEmbed.timestamp = new Date();
+			await message.channel.send("", {embed: helpEmbed});
+		}
+
 		const helpMsg = message.channel.id == '382701090430386180'
 			? `To search for a guide, use **.${exports.help.name}** <keyword>.`
 			: `To search for a guide, use **.${exports.help.name}** <keyword> in the <#382701090430386180> channel.`
