66949a2dc4599c0d221ad8de46cf49dc3b1e1235
Ethan Smith <ethanverdellsmith@gmail.com>
2018-09-07 16:22:55 -0400

    added bak bolts and fixed feathers

--- guides/dailyGuide.js
+++ guides/dailyGuide.js
@@ -1,29 +1,52 @@
 exports.data = {
-	"help": {
-			"title": "Comprehensive list of all valid commands",
-			"author": {
-				"name": "Cape Info",
-				"icon_url": "https://i.imgur.com/6c6q2iC.png"
-			},
-			"description": "{{output}}",
-			"footer": {
-				"icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
-				"text": "Achievement Help"
-			}
-	},
-	"search": {
-			"title": "All commands matching your search",
-			"author": {
-				"name": "Cape Info",
-				"icon_url": "https://i.imgur.com/6c6q2iC.png"
-			},
-			"description": "{{output}}",
-			"footer": {
-				"icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
-				"text": "Achievement Help"
-			}
-	},
-	"broad arrowheads": {
+  "help": {
+      "title": "Comprehensive list of all valid commands",
+      "author": {
+        "name": "Cape Info",
+        "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+      "description": "{{output}}",
+      "footer": {
+        "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+        "text": "Achievement Help"
+      }
+  },
+  "search": {
+      "title": "All commands matching your search",
+      "author": {
+        "name": "Cape Info",
+        "icon_url": "https://i.imgur.com/6c6q2iC.png"
+      },
+      "description": "{{output}}",
+      "footer": {
+        "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+        "text": "Achievement Help"
+      }
+  },
+  "bakriminel bolts": {
+    "title": "Bakriminel Bolts",
+    "description": "[Bakriminel Bolts](runescape.wikia.com/wiki/Bakriminel_bolts) require 93 Fletching to make and can be made from [Bloodwood Logs](http://runescape.wikia.com/wiki/Bloodwood_logs).\n\n[Bloodwood trees](https://runescape.wikia.com/wiki/Bloodwood_tree) typically yield a very small number of logs, usually 1-2 per yield. However, those in the [Wilderness](http://runescape.wikia.com/wiki/Wilderness) are significantly stronger and are likely to give more logs compared to their non-Wilderness counterparts.\n\nA list of tree locations can be found [here](http://runescape.wikia.com/wiki/Bloodwood_tree#Locations). Note that 3 of the trees that are not locked behind quests are in the wilderness, so **do not** take anything you're not willing to lose.",
+    "url": "http://runescape.wikia.com/wiki/Crystal_flask",
+    "color": 12269891,
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/f/f8/Bakriminel_bolts_detail.png/revision/latest?cb=20120510191137"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+     "fields": [
+      {
+        "name": "Useful Information:",
+        "value": "The logs and bolt shafts themselves cannot be taken too far from the tree, or they will turn into dust. "
+      }
+    ]
+  },
+  "broad arrowheads": {
     "title": "Broad Arrowheads",
     "description": "[Broad Arrowheads](http://runescape.wikia.com/wiki/Broad_arrowheads) can be purchased only after the completion of [Smoking Kills](http://runescape.wikia.com/wiki/Smoking_Kills) from [Slayer Shops](http://runescape.wikia.com/wiki/Slayer_Equipment).\n\nA total of 9,000 arrowheads can be purchased daily. 3,000 from [Turael](http://runescape.wikia.com/wiki/Turael) **or** [Spria](http://runescape.wikia.com/wiki/Spria). Note that they share the same stock), 3,000 from any other [Slayer Master](http://runescape.wikia.com/wiki/Slayer_Master). An additional 3,000 can also be purchased from [Noterazzo](http://runescape.wikia.com/wiki/Noterazzo), found in the [Wilderness Bandit Camp](http://runescape.wikia.com/wiki/Bandit_Camp_(Wilderness).",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_broad_arrowheads",
@@ -45,7 +68,7 @@ exports.data = {
       }
     ]
   },
-	"crystal flasks": {
+  "crystal flasks": {
     "title": "Crystal Flasks",
     "description": "[Crystal Flasks](http://runescape.wikia.com/wiki/Crystal_flask) require the completion of [Plague's End](http://runescape.wikia.com/wiki/Plague%27s_End). They can be made from [Crystal-flecked Sandstone](http://runescape.wikia.com/wiki/Crystal-flecked_sandstone).\n\n50 sandstone can be mined daily in the [Ithell District](http://runescape.wikia.com/wiki/Ithell_Clan), and an additional 25 in the [Edimmu Resource Dungeon](http://runescape.wikia.com/wiki/Edimmu_resource_dungeon), requiring 115 Dungeoneering. To operate the machine, a player must have completed the [As a First Resort](http://runescape.wikia.com/wiki/As_a_First_Resort) quest.",
     "url": "http://runescape.wikia.com/wiki/Crystal_flask",
@@ -67,10 +90,11 @@ exports.data = {
       }
     ]
   },
-	"feathers": {
+  "feathers": {
     "title": "Feathers",
-    "description": "[Feathers](http://runescape.wikia.com/wiki/Feather) are a great way to make some money, especially if you are new or have very few coins to begin with. A full list of locations and the preferred route can be found [here](http://runescape.wikia.com/wiki/Money_making_guide/Buying_feathers).",
+    "description": "[Feathers](http://runescape.wikia.com/wiki/Feather) are a great way to make some money, especially if you are new or have very few [coins](https://runescape.wikia.com/wiki/Coins) to begin with.\n\n1,000 feathers can be bought daily, along with the recent addition of [feather packs](http://runescape.wikia.com/wiki/Feather_pack), which can increase the number purchased per day.\n\nA full list of locations and the preferred route can be found [here](http://runescape.wikia.com/wiki/Money_making_guide/Buying_feathers).",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_feathers",
+    "color": 12269891,
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
       "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
@@ -79,19 +103,19 @@ exports.data = {
       "url": "https://vignette.wikia.nocookie.net/runescape2/images/3/3d/Feather_detail.png/revision/latest?cb=20111121001036"
     },
     "author": {
-      "name": "Daily Money Making Guide Info",
+      "name": "Max Cape Info",
       "icon_url": "https://i.imgur.com/6c6q2iC.png"
     },
      "fields": [
       {
         "name": "Useful Information:",
-        "value": "Feathers will always be in demand, as they are used for Fletching and Fishing. A player only needs 66,000 to buy out all shops of their feathers."
+        "value": "Feathers will always be in demand, as they are used for Fletching and Fishing. When it comes to the feather packs, the full stock does not replenish each day. Instead, each store's stock of feather packs replenishes at a rate of about 1 per 12 hours of real time, independently. Keep in mind that [Gerrant's Fishy Business](http://runescape.wikia.com/wiki/Gerrant%27s_Fishy_Business) and [Lovecraft's Tackle](http://runescape.wikia.com/wiki/Lovecraft%27s_Tackle) share the same stock."
       }
     ]
   },
-	"potion flasks": {
+  "potion flasks": {
     "title": "Potion Flasks",
-    "description": "[Potion Flasks](http://runescape.wikia.com/wiki/Potion_flask) can be made from [Red Sandstone](http://runescape.wikia.com/wiki/Red_sandstone).\n\n50 sandstone can be mined daily north of [Oo'glog](http://runescape.wikia.com/wiki/Oo%27glog), and an additional 25 east of [Sophanem](runescape.wikia.com/wiki/Sophanem) after the completion of the [Elite Desert Tasks](http://runescape.wikia.com/wiki/Desert_achievements).",
+    "description": "[Potion Flasks](http://runescape.wikia.com/wiki/Potion_flask) can be made from [Red Sandstone](http://runescape.wikia.com/wiki/Red_sandstone).\n\n50 sandstone can be mined daily north of [Oo'glog](http://runescape.wikia.com/wiki/Oo%27glog), and an additional 25 east of [Sophanem](https://runescape.wikia.com/wiki/Sophanem) after the completion of the [Elite Desert Tasks](http://runescape.wikia.com/wiki/Desert_achievements).",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Making_potion_flasks",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -111,9 +135,9 @@ exports.data = {
       }
     ]
   },
-	"rune shop run": {
+  "rune shop run": {
     "title": "Rune Shop Run",
-    "description": "Runes can be purchased from [Magic Shops](http://runescape.wikia.com/wiki/Magic_shops) daily. Players should only be purchasing the following runes per day:\n\n• [Body Rune](http://runescape.wikia.com/wiki/Body_rune)\n• [Air Rune](http://runescape.wikia.com/wiki/Air_rune)\n• [Water Rune](http://runescape.wikia.com/wiki/Water_rune)\n• [Earth Rune](http://runescape.wikia.com/wiki/Earth_rune)\n• [Fire Rune](http://runescape.wikia.com/wiki/Fire_rune)\n• [Nature Rune](runescape.wikia.com/wiki/Nature_rune)",
+    "description": "Runes can be purchased from [Magic Shops](http://runescape.wikia.com/wiki/Magic_shops) daily. Players should only be purchasing the following runes per day:\n\n• [Body Rune](http://runescape.wikia.com/wiki/Body_rune)\n• [Air Rune](http://runescape.wikia.com/wiki/Air_rune)\n• [Water Rune](http://runescape.wikia.com/wiki/Water_rune)\n• [Earth Rune](http://runescape.wikia.com/wiki/Earth_rune)\n• [Fire Rune](http://runescape.wikia.com/wiki/Fire_rune)\n• [Nature Rune](https://runescape.wikia.com/wiki/Nature_rune)",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_runes",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -133,7 +157,7 @@ exports.data = {
       }
     ]
   },
-	"vis wax": {
+  "vis wax": {
     "title": "Rune Goldberg Machine - Vis Wax",
     "description": "The [Rune Goldberg Machine](http://runescape.wikia.com/wiki/Rune_Goldberg_Machine) is a [Distraction and Diversion](http://runescape.wikia.com/wiki/Distractions_and_Diversions) that involves turning [Runes](http://runescape.wikia.com/wiki/Runes) into [Vis Wax](http://runescape.wikia.com/wiki/Vis_wax).\n\nThe machine is located in the [Runecrafting Guild](http://runescape.wikia.com/wiki/Runecrafting_Guild). It's required to have at least level 50 Runecrafting in order to enter the guild. \n\nEach of the three slots has a **best** rune, which changes daily. The first slot is the same for every player. The second slot is one of three possible runes (varies from player to player). The third slot is randomly assigned and unique to each player. Using the [Runecrafting Cape](http://runescape.wikia.com/wiki/Runecrafting_cape) is the best way to know your own unique third slot.",
     "url": "http://runescape.wikia.com/wiki/Rune_Goldberg_Machine",
@@ -155,7 +179,7 @@ exports.data = {
       }
     ]
   },
-	"yak hides": {
+  "yak hides": {
     "title": "Yak-hides",
     "description": "[Yak-hide](http://runescape.wikia.com/wiki/Yak-hide) can be purchased after the completion of [The Fremennik Isles](http://runescape.wikia.com/wiki/The_Fremennik_Isles) quest. 1,000 hides can be bought daily from [Vanligga Gastfrihet](http://runescape.wikia.com/wiki/Vanligga_Gastfrihet) in [Jatizso](https://runescape.wikia.com/wiki/Jatizso). ",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_yak-hide",
