8cbace980a497f583b2f98d2ab17a7b186ea30f7
Ethan Smith <ethanverdellsmith@gmail.com>
2018-09-07 12:01:04 -0400

    minor QA

--- guides/dailyGuide.js
+++ guides/dailyGuide.js
@@ -25,7 +25,7 @@ exports.data = {
 	},
 	"broad arrowheads": {
     "title": "Broad Arrowheads",
-    "description": "[Broad Arrowheads](http://runescape.wikia.com/wiki/Broad_arrowheads) can be purchased only after the completion of [Smoking Kills](http://runescape.wikia.com/wiki/Smoking_Kills) from [Slayer Shops](http://runescape.wikia.com/wiki/Slayer_Equipment). A total of 9,000 arrowheads can be purchased daily. 3,000 from [Turael](http://runescape.wikia.com/wiki/Turael) or [Spria](http://runescape.wikia.com/wiki/Spria) (They share the same stock), 3,000 from any other [Slayer Master](http://runescape.wikia.com/wiki/Slayer_Master), and 3,000 from [Noterazzo](http://runescape.wikia.com/wiki/Noterazzo) found in the [Wilderness Bandit Camp](http://runescape.wikia.com/wiki/Bandit_Camp_(Wilderness).",
+    "description": "[Broad Arrowheads](http://runescape.wikia.com/wiki/Broad_arrowheads) can be purchased only after the completion of [Smoking Kills](http://runescape.wikia.com/wiki/Smoking_Kills) from [Slayer Shops](http://runescape.wikia.com/wiki/Slayer_Equipment).\n\nA total of 9,000 arrowheads can be purchased daily. 3,000 from [Turael](http://runescape.wikia.com/wiki/Turael) **or** [Spria](http://runescape.wikia.com/wiki/Spria). Note that they share the same stock), 3,000 from any other [Slayer Master](http://runescape.wikia.com/wiki/Slayer_Master). An additional 3,000 can also be purchased from [Noterazzo](http://runescape.wikia.com/wiki/Noterazzo), found in the [Wilderness Bandit Camp](http://runescape.wikia.com/wiki/Bandit_Camp_(Wilderness).",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_broad_arrowheads",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -40,14 +40,14 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
+        "name": "Useful Information:",
         "value": "Broad arrowheads are one of the most popular ways to train Fletching, so this daily run will always generate income. **Do not** take anything into the wilderness that you aren't prepared to lose."
       }
     ]
   },
 	"crystal flasks": {
     "title": "Crystal Flasks",
-    "description": "[Crystal Flasks](http://runescape.wikia.com/wiki/Crystal_flask) require the completion of [Plague's End](http://runescape.wikia.com/wiki/Plague%27s_End). They can be made from [Crystal-flecked Sandstone](http://runescape.wikia.com/wiki/Crystal-flecked_sandstone). 50 sandstone can be mined daily in the [Ithell District](http://runescape.wikia.com/wiki/Ithell_Clan), and an additional 25 in the [Edimmu Resource Dungeon](http://runescape.wikia.com/wiki/Edimmu_resource_dungeon) (requires 115 Dungeoneering). To operate the machine, a player must have completed [As a First Resort](http://runescape.wikia.com/wiki/As_a_First_Resort) quest",
+    "description": "[Crystal Flasks](http://runescape.wikia.com/wiki/Crystal_flask) require the completion of [Plague's End](http://runescape.wikia.com/wiki/Plague%27s_End). They can be made from [Crystal-flecked Sandstone](http://runescape.wikia.com/wiki/Crystal-flecked_sandstone).\n\n50 sandstone can be mined daily in the [Ithell District](http://runescape.wikia.com/wiki/Ithell_Clan), and an additional 25 in the [Edimmu Resource Dungeon](http://runescape.wikia.com/wiki/Edimmu_resource_dungeon), requiring 115 Dungeoneering. To operate the machine, a player must have completed the [As a First Resort](http://runescape.wikia.com/wiki/As_a_First_Resort) quest.",
     "url": "http://runescape.wikia.com/wiki/Crystal_flask",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -62,7 +62,7 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
+        "name": "Useful Information:",
         "value": "The crystal flasks are popular due to the fact that they hold 2 more doses of a potion. They are also used for making combination potions."
       }
     ]
@@ -84,14 +84,14 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
+        "name": "Useful Information:",
         "value": "Feathers will always be in demand, as they are used for Fletching and Fishing. A player only needs 66,000 to buy out all shops of their feathers."
       }
     ]
   },
 	"potion flasks": {
     "title": "Potion Flasks",
-    "description": "[Potion Flasks](http://runescape.wikia.com/wiki/Potion_flask) can be made from [Red Sandstone](http://runescape.wikia.com/wiki/Red_sandstone). 50 sandstone can be mined daily north of [Oo'glog](http://runescape.wikia.com/wiki/Oo%27glog), and an additional 25 east of [Sophanem](runescape.wikia.com/wiki/Sophanem) after the completion of the [Elite Desert Tasks](http://runescape.wikia.com/wiki/Desert_achievements).",
+    "description": "[Potion Flasks](http://runescape.wikia.com/wiki/Potion_flask) can be made from [Red Sandstone](http://runescape.wikia.com/wiki/Red_sandstone).\n\n50 sandstone can be mined daily north of [Oo'glog](http://runescape.wikia.com/wiki/Oo%27glog), and an additional 25 east of [Sophanem](runescape.wikia.com/wiki/Sophanem) after the completion of the [Elite Desert Tasks](http://runescape.wikia.com/wiki/Desert_achievements).",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Making_potion_flasks",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -106,7 +106,7 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
+        "name": "Useful Information:",
         "value": "The potion flasks are popular due to the fact that they hold 2 more doses of a potion."
       }
     ]
@@ -128,14 +128,14 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
+        "name": "Useful Information:",
         "value": "The recommended route and profit per instance can be found [here](http://runescape.wikia.com/wiki/Money_making_guide/Buying_runes). Keep in mind although some areas have requirements, it may still be worth visiting the shops you're able to daily as the coins add up."
       }
     ]
   },
 	"vis wax": {
     "title": "Rune Goldberg Machine - Vis Wax",
-    "description": "The [Rune Goldberg Machine](http://runescape.wikia.com/wiki/Rune_Goldberg_Machine) is a [Distraction and Diversion](http://runescape.wikia.com/wiki/Distractions_and_Diversions) that involves turning [Runes](http://runescape.wikia.com/wiki/Runes) into [Vis Wax](http://runescape.wikia.com/wiki/Vis_wax). The machine is located in the [Runecrafting Guild](http://runescape.wikia.com/wiki/Runecrafting_Guild). It's required to have at least level 50 Runecrafting in order to enter the guild.  Each of the three slots has a **best** rune, which changes daily. The first slot is the same for every player. The second slot is one of three possible runes (varies from player to player). The third slot is randomly assigned and unique to each player. Using the [Runecrafting Cape](http://runescape.wikia.com/wiki/Runecrafting_cape) is the best way to know your own unique third slot.",
+    "description": "The [Rune Goldberg Machine](http://runescape.wikia.com/wiki/Rune_Goldberg_Machine) is a [Distraction and Diversion](http://runescape.wikia.com/wiki/Distractions_and_Diversions) that involves turning [Runes](http://runescape.wikia.com/wiki/Runes) into [Vis Wax](http://runescape.wikia.com/wiki/Vis_wax).\n\nThe machine is located in the [Runecrafting Guild](http://runescape.wikia.com/wiki/Runecrafting_Guild). It's required to have at least level 50 Runecrafting in order to enter the guild. \n\nEach of the three slots has a **best** rune, which changes daily. The first slot is the same for every player. The second slot is one of three possible runes (varies from player to player). The third slot is randomly assigned and unique to each player. Using the [Runecrafting Cape](http://runescape.wikia.com/wiki/Runecrafting_cape) is the best way to know your own unique third slot.",
     "url": "http://runescape.wikia.com/wiki/Rune_Goldberg_Machine",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -150,14 +150,14 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
-        "value": "The command **!vis** in <#382701090430386180> can be used to find out the days combinations. To make the most out of your coins, it's recommended to use the runes purchased from a [Rune Shop](http://runescape.wikia.com/wiki/Magic_shops) run. It is also recommended to stick to elemental runes, as they tend to generate the most profit. A full list of runes and their profit potential can be found [here](http://runescape.wikia.com/wiki/Rune_Goldberg_Machine)."
+        "name": "Useful Information:",
+        "value": "The command **!vis** in <#382701090430386180> can be used to find out the days combinations. To make the most out of your coins, it's recommended to use the runes purchased from a [Rune Shop](http://runescape.wikia.com/wiki/Magic_shops) run.\n\nIt is also recommended to stick to elemental runes, as they tend to generate the most profit. A full list of runes and their profit potential can be found [here](http://runescape.wikia.com/wiki/Rune_Goldberg_Machine)."
       }
     ]
   },
 	"yak hides": {
     "title": "Yak-hides",
-    "description": "[Yak-hide](http://runescape.wikia.com/wiki/Yak-hide) can be purchased after the completion of [The Fremennik Isles](http://runescape.wikia.com/wiki/The_Fremennik_Isles) quest. 1,000 hides can be bought daily from [Vanligga Gastfrihet](http://runescape.wikia.com/wiki/Vanligga_Gastfrihet) in Jatizso. ",
+    "description": "[Yak-hide](http://runescape.wikia.com/wiki/Yak-hide) can be purchased after the completion of [The Fremennik Isles](http://runescape.wikia.com/wiki/The_Fremennik_Isles) quest. 1,000 hides can be bought daily from [Vanligga Gastfrihet](http://runescape.wikia.com/wiki/Vanligga_Gastfrihet) in [Jatizso](https://runescape.wikia.com/wiki/Jatizso). ",
     "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_yak-hide",
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
@@ -172,8 +172,8 @@ exports.data = {
     },
      "fields": [
       {
-        "name": "Useful Information",
-        "value": "Yak-hides are used often for Summoning. Using the [Mobile Perk](http://runescape.wikia.com/wiki/Mobile) can help speed up the buying process. A player only needs 50,000 to buy out the shop of the hides."
+        "name": "Useful Information:",
+        "value": "Yak-hides are used often for Summoning. Using the [Mobile Perk](http://runescape.wikia.com/wiki/Mobile) can help speed up the buying process.\n\n A player only needs 50,000 [coins](https://runescape.wikia.com/wiki/Coins) to buy out the shop of the hides."
       }
     ]
   }
