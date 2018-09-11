ba680a4ee9c4ed152a27bbcb3a72fd703940a595
Maximillion <maximillionrh@gmail.com>
2018-09-11 11:35:29 +0100

    new files for info channels/commands

---
+++ info/events.js
@@ -0,0 +1,12 @@
+exports.data = {
+    "General Graardor": ,
+    "Gregorovic": ,
+    "Helwyr": ,
+    "Mega Duck": ,
+    "Nex": ,
+    "Nex AOD": ,
+    "Trouble Brewing": ,
+    "Twin Furies": ,
+    "Vindicta": ,
+    "Vorago": 
+};
