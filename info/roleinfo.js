1ce9064988330d8e4959bdf002d873cdfa232d97
Ethan Smith <ethanverdellsmith@gmail.com>
2018-09-12 14:55:03 -0400

    fully uploaded role file (needs headers)

--- info/roleinfo.js
+++ info/roleinfo.js
@@ -1,6 +1,142 @@
 exports.data = {
-    "roles information": ,
-    "runescape notify roles": ,
-    "dsf merchant notify roles": ,
-    "discord server notify roles": 
+    "staff assigned roles": {
+    "title": "Staff Assigned Roles",
+    "description":"Achievement Help is a friendly community for those looking to complete and help each other with all kinds of achievements. The following roles **may not** be self assigned, as they represent achievements earned in game or inside the discord.",
+    "thumbnail": {
+      "url": "https://media.discordapp.net/attachments/413750063756607488/466104364398936065/Roles.png"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Role Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "**<@&382703090455019521>**\n• We try our best to bring the community together and provide a platform for people to assist one another. The council collectively maintain public relations and outreach, maintain server channels and information, host events, and provide assistance where able\n\n**<@&382705157709758466>**\n• Our moderators assist in keeping the server clean and moderated, and can often help you should you have anything server-related on your mind. You can also ask them to assign a rank if you have earned any of the below roles in game!\n\n**<@&382710482232016896>**\n• Contributors are people who have proven themselves to be helpful not only in helping others by being able to provide reliable and accurate information, but also in maintaining the server or organizing teams to help with achievements requiring a group."
+      },
+      {
+        "name": "​",
+        "value": "**<@&452447822353268737>**\n• Reaper Coordinators are members of the <@&415885607626211338> who have gone above and beyond in their aid of those who have asked for assistance in the <#436714722847621120>. They are responsible for managing the list and ensuring Reaper kills are done smoothly and without incident.\n\n**<@&382711210044555264>**\n• This role is bestowed to members who have completed **all** RuneScape Activities listed in the True Trim compilation list.\n\n**<@&426857028221140994>**\n• This role is bestowed to members who have completed **all** Achievements in the Achievement interface in game.\n\n**<@&399992602658603008>**\n• This role is bestowed to members who have the max amount of RuneScore points."
+      },
+      {
+        "name": "​",
+        "value": "**<@&399992483154755585>**\n• This role is bestowed to members who have at least 92% achievements completed or 92% of the current RuneScore.\n\n**<@&383563659391270912>**, **<@&382711161713590272>**, **<@&382711127718494210>**, and **<@&382710741725347850>**\n• These roles are bestowed if you are able to obtain ownership of the corresponding cape within the game. The rank does not expire when new content is released.\n\n**<@&400738052512940043>**\n• This role is given to members who have obtained the Master of all title in game for collecting all skilling and combat related pets.\n\n**<@&400739452663758850>**\n• This role is given to members who have obtained the Insane Final Boss title in game for collecting all boss related pets and having at least 100 KC of every normal and hard/challenge mode boss.\n\n**<@&408381118920917012>**\n• This role is given to members who have obtained the maximum amount of experience in the game (currently 5,400,000,000)."
+      },
+      {
+        "name": "​",
+        "value": "**<@&409917475157704704>**\n• This role is given to members who have obtained the maximum total virtual skill level. (Currently 3,270)\n\n**<@&403987258404765697>**\n• This role is given to members who **actively** play on a Hardcore Ironman account, whether it is their main or alternative account.\n\n**<@&403987115743903786>**\n• This role is given to members who **actively** play on a Ironman account, whether it is their main or alternative account.\n\n**<@&436706353856249869>**\n• This role is for members who were participants in Achievement Help's weekly <#418881020071247880>. This title will be removed and redistributed upon the next event.\n\n**<@&431238030716698626>**\n• The first player with the most cumulative points for each update which adds RuneScore will be granted the Achievement Champion title with a color of their choice. Points will be assigned based on the RuneScore value associated to the completed achievement. This title passes on to the next valid bearer."
+      },
+      {
+        "name": "​",
+        "value": "**<@&431238109817208842>**\n• Every time an Achievement Help member wins the Achievement Champion title for the 10th time, they will be awarded the GaMeBrEaKeR title. This title passes on to the next valid bearer.\n\n**<@&415885607626211338>**\n• This role is for members who are dedicated to helping players through their first kill of any non-solo boss for the Completionist Cape requirement. This role is not to be tagged for normal bossing sessions, we have <#477196003989127168> for that purpose. This is not a leeching service, the members of this role are confident in their ability to guide you through your participation in a successful kill.\n\n**<@&425074526116904965>**\n• This role is for members who wish to help Saint Cannon with the organization and execution of Achievement Help events.\n\n**<@&399291635898253333>**\n• This role is bestowed to members who lead an affilated server that is in partnership with Achievement Help."
+      },
+      {
+        "name":"​",
+        "value": "**<@&382710614222569482>**\n• This is for members who have shown strong activity in the Achievement Help Discord Community and its conversations.\n\n**<@&399992283488845845>**\n• This is for members who have been verified to have 50+ followers on either YouTube or Twitch."
+      }      
+      ]
+  },
+    "runescape notify roles": {
+    "title": "RuneScape Notify Roles (Self-Assigned)",
+    "description":"The notify roles are meant to be used in order to help one another with various achievements.\n• To join a notify role, simply react to the proper message in <#465998663710146579> with the corresponding emoji.\n• You may also send ?rank [role] in <#382701090430386180>.\n\t- **Example:** `?rank AFK Thaler`\n\t- Leaving a role is the same command as joining that role.",
+    "thumbnail": {
+      "url": "https://media.discordapp.net/attachments/413750063756607488/466104364398936065/Roles.png"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Role Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "**<@&382940086511075349>**\n• **When to tag:** If you have a Consistent Yak Card (team) Gorajo card to offer.\n\n• **When to join:** If you would like to be notified when a Consistent Yak Card (team) is being offered.\n\n**<@&382959993768378378>**\n• **When to tag:** If you need a conquest partner.\n\n• **When to join:** If you would like to be notified when someone is looking for a Conquest Partner.\n\n**<@&382960017499881474>**\n• **When to tag:** If you are looking for a hard mode Dungeoneering partner. This is not for a normal Dungeoneering team.\n\n •**When to join:**  If you would like to be notified when someone is looking for a hard mode Dungeoneering partner.\n\n**<@&382960080125165568>**\n• **When to tag:** If you are looking for a team for the Barbarian Assault Comp for Trim Requirements.\n\n• **When to join:** If you would like to be notified when someone is looking for a team for the Barbarian Assault Comp for Trim Requirements."
+      },
+      {
+        "name": "​",
+        "value": "**<@&382960099204792320>**\n• **When to tag:** If you need a team for Mobilizing Armies, preferably only during spotlight.\n\n• **When to join:** If you would like to be notified when someone needs a team for Mobilizing Armies.\n\n**<@&382957843927662602>**\n• **When to tag:** If you are a Raid affiliates or team that needs a +1.\n\n• **When to join:** If you would like to be notified of a Raid affiliates or team that needs a +1, though more consistent teams can be found with our Raid <#477196003989127168>.\n\n**<@&383654949957009428>**\n• **When to tag:** If you have a question for those who have gone above and beyond the game's demands.\n\n• **When to join:** If you’d like to be identified among players who can work together on this extreme goal.\n\n**<@&420015337271066674>**\n• **When to tag:** If you have spotted a corrupted egg in Menaphos.\n\n• **When to join:** If you would like to be notified when a player has spotted a corrupted egg in Menaphos."
+      },
+      {
+        "name": "​",
+        "value": "**<@&420015391830573056>**\n• **When to tag:** If you have spotted a crystal impling.\n\n• **When to join:** If you would like to be notified when a player has spotted a crystal impling.\n\n**<@&420015949287260161>**\n• **When to tag:** If you would like to complete a Reaper assignment with a duo.\n\n• **When to join:** If you would like to be notified when someone is wanting to do a Reaper assignment with a duo.\n\n**<@&420024867858022403>**\n• **When to tag:**  If you have spotted the Prifddinas Raven.\n\n• **When to join:** If you would like to be notified when someone has spotted the Prifddinas Raven.\n\n**<@&420024943481454592>**\n• **When to tag:** If you are looking for a partner to do IFB challenges with.\n\n• **When to join:** If you would like to be notified when someone is looking for a partner to do IFB challenges with.\n\n**<@&420028242401951744>**\n• **When to tag:** If you have spotted Krar Jnr.\n\n• **When to join:** If you would like to be notified when someone has spotted Krar Jnr."
+      },
+      {
+        "name": "​",
+        "value": "**<@&420329678285438986>**\n• **When to tag:** If you are looking for Dungeoneering Teams.\n\n• **When to join:** If you would like to be notified when someone is looking for Dungeoneering Teams.\n\n**<@&420029633371570176>**\n• **When to tag:** If you have golden crackers to pull.\n\n• **When to join:** If you would like to be notified when someone has golden crackers to pull.\n\n**<@&451411977894494212>**\n• **When to tag:** If you are looking for people to help with a group minigame related achievement or song unlock.\n\n• **This is not for Thaler purposes**\n\n• **When to join:** If you are in need of a group minigame related achievement or song unlock and would like to be notified when groups are forming.\n• If you are willing to help those who are in need of a minigame related achievement or song unlock."
+      },
+      {
+        "name": "​",
+        "value": "**<@&451412074334126091>**\n• **When to tag:** If you are looking for people to help with a stat boost for quests, skilling, or effigies.\n\n• **When to join:**If you would like to be notified when someone is looking for people to help with a stat boost for quests, skilling, or effigies.\n\n**<@&425025597635428376>**\n• **When to tag:** If you are looking to complete the achievement Open Bar, and would like to have the 28 ales temporarily traded to you.\n\n• Not returning these ales will result in a ban from the server.\n\n• **When to join:** If you are in possession of the 28 different ales, and would like to help others who are looking to complete the achievement.\n\n**<@&426195054827274240>**\n• **When to tag:** If you have an Elite Rune Dragon spawn.\n• Please list the price of the sell and distinguish if it is a normal elite or Barry.\n\n• **When to join:** If you would like to be notified when someone has an Elite Rune Dragon spawn."
+      },
+      {
+        "name": "​",
+        "value": "**<@&426195111148388372>**\n• **When to tag:** If you have an Elite Edimmu spawn.\n\n• **When to join:** If you would like to be notified when someone has an Elite Edimmu spawn.\n\n**<@&430138114455896074>**\n• **When to tag:** If you need help with a Penance King kill.\n\n• **When to join:** If you would like to be notified when someone needs a team for a Penance King kill.\n\n**<@&446729279149965315>**\n• **When to tag:** If you are looking to organize a group of people to AFK the currently spotlighted minigame.\n\n• **This is not for Minigame Achievement/Song Help purposes.**\n\n• **When to join:** If you would like to be notified of groups being created to AFK the currently spotlighted minigame.\n\n**<@&488765560541413376>**\n• **When to tag:**  If you are looking to organize a group of Ironman/woman to complete achievements and bossing.\n\n• **When to join:** If you would like to be notified of groups being created by an Ironman/woman to complete achievements and bossing."
+      },
+      {
+        "name": "​",
+        "value": "**<@&488765489280188437>**\n• **When to tag:** If you are looking to organize a group of to obtain the Survive Title at the Dominion Tower.\n\n• **When to join:** If you would like to be notified of groups being created to obtain the Survive Title at the Dominion Tower.\n\n**<@&488492228512317451>**\n• **When to tag:** If you are looking to complete a PoF achievement and would like to see if anyone has the items to assist you with.\n\n• Not returning these items will result in a ban from the server.\n\n• **When to join:** If you are in possession of PoF achievement items, like 12 bunnies or 4 seasonal rams, and would like to help others who are looking to complete the achievement."
+      }
+      ]
+  },
+    "dsf merchant notify roles": {
+    "title": "Deep Sea Fishing Merchant Roles",
+    "description":"The following roles were added for the Deep Sea Fishing Merchant because of their correlation to current or future Achievements.\n\nThe following roles can also be added in one of the following ways;\n• To join a DSF Merchant role, simply react to the proper message in <#465998663710146579> with the corresponding emoji.\n• You may also send ?rank [role] in <#382701090430386180>.\n\t- **Example:** `?rank AFK Thaler`\n\t- Leaving a role is the same command as joining that role.\n\nIf you would like to have other notifications besides these, you can use <@439803413623078927> in the <#382701090430386180> channel. Simply type `?addnotif Item Name Here`, and the bot will PM you whenever that item is in the store.",
+    "thumbnail": {
+      "url": "https://media.discordapp.net/attachments/413750063756607488/466104364398936065/Roles.png"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Role Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "**<@&443761366201925653>**\n• 20 Reaper Points to go towards the Trimmed cape requirement.\n\n**<@&443761702903873536>**\n• 5,000 Runespan Points to go towards the Trimmed cape requirement.\n\n**<@&443762022178488320>**\n• 350 (Small), 900 (Medium), or 1350 (Large) Menaphos Reputation to go towards the Trimmed cape requirement or for Quest unlocks.\n\n**<@&444155849791766528>**\n•  500,000 - 2,000,000\n\n**<@&443762434818179073>**\n• 10,000 - 40,000 Livid Farm Produce Points to go towards the Completionist cape requirement.\n\n**<@&443762151228833794>**\n• 3 - 5 Taijitu to go towards the Arc's Waiko Reward Shop requirements for an array of all cape requirements.\n\n**<@&443762202235764738>**\n• 1 enhancer to go towards an array of all cape requirements."
+      },
+      {
+        "name": "​",
+        "value": "**<@&443772051258605578>**\n• 1 enhancer to go towards an array of all cape requirements.\n\n**<@443762475381555211&>**\n• 50 (Small), 100 (Normal), or 150 (Large) Mazcab Reputation to go towards the 5,000 required reputation.\n\n**<@&443762987782897674>**\n• 500 God Wars Dungeon 2 Reputation to go towards the 20,000 required reputation.\n\n**<@&443794309242945536>**\n• 50 - 100 Silverhawk Down for Max Cape purposes.\n\n**<@&443761808478830593>**\n• 1 - 3 cards for Max Cape purposes (50% extra Dungeonering experience per floor)\n\n**<@&443794570791223317>**\n• Experience rewards for Max Cape purposes.\n\n**<@&443794600705130496>**\n• For an array of all cape requirements."
+      },
+      {
+        "name": "​",
+        "value": "**<@&443794634586718218>**\n• For an array of all cape requirements.\n\n**<@&443794673052680203>**\n• For an array of all cape requirements."
+      }
+      ]
+  },
+    "discord server notify roles": {
+    "title": "Achievement Help Discord Roles",
+    "description":"These roles are only to be called by Contributors, Moderators, or Council. Make sure not to abuse notifying these roles. If you keep mentioning these roles for no reason, a mute may be applied.\n\nThe following roles can also be added in one of the following ways;\n• To join a DSF Merchant role, simply react to the proper message in <#465998663710146579> with the corresponding emoji.\n• You may also send ?rank [role] in <#382701090430386180>.\n\t- **Example:** `?rank AFK Thaler`\n\t- Leaving a role is the same command as joining that role.",
+    "thumbnail": {
+      "url": "https://media.discordapp.net/attachments/413750063756607488/466104364398936065/Roles.png"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Role Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "**<@&420015457110720523>**\n• **When to join:** If you would like to be notified when an Achievement Help Poll is occurring.\n\n**<@&420015552677937152>**\n• **When to join:** If you would like to be notified when an Achievement Help Event has been created or begun.\n\n**<@&420015659620237312>**\n• **When to join:** If you would like to be notified when an Affiliate Event has been created or begun.\n\n**<@&420015717828657152>**\n• **When to join:** If you would like to be notified when an Official RuneScape Event has been created or begun.\n\n**<@&420015773621420043>**\n• **When to join:** If you would like to be notified when the Achievement Help Discord has been updated."
+      },
+      {
+        "name": "​",
+        "value": "**<@&420015843804708864>**\n• **When to join:** If you would like to be notified whenever something is posted in <#395575375083601920>\n\n**<@&431292159053004800>**\n• **When to join:** If you would like to be notified when the official RuneScape stream or a verified streamer from the Achievement Help Discord goes live.\n\n**<@&431292311553572864>**\n• **When to join:** If you would like to be notified of when game news is posted to the official RuneScape website.\n\n**<@&431292243140280320>**\n• **When to join:** If you would like to be notified of the spotlight change as it occurs."
+      }
+      ]
+  }
 };
