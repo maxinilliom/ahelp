b99d1b752eafa663e90248eeb3ff69dee37c73d1
Maximillion <maximillionrh@gmail.com>
2018-09-15 17:24:22 +0100

    remove thumbnails

--- info/fc.js
+++ info/fc.js
@@ -32,9 +32,6 @@ exports.data = {
   "fctext": {
     "title": "Looking For More!",
     "description":"<:bulletpoint:418304110429732883> Please forward any friend's chat suggestions to us and we'll add them to the following lists.\n<:bulletpoint:418304110429732883> You can join Achievement Help’s Official friend chat Saint Cannon in game!",
-    "thumbnail": {
-      "url": "https://vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest?cb=20140317195258"
-    },
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
       "text": "Achievement Help | Friend Chats"
@@ -59,9 +56,6 @@ exports.data = {
     "minigames": {
     "title": "Minigames",
     "description":"**Cwars United** - Castle Wars Games (Afk). Apply [Here](http://services.runescape.com/m=forum/c=ob7A0kMf9cA/forums.ws?75,76,32,66000897).\n\n**Minigames** - Mazcab Supply Runs and All Around Minigame Needs\n\n**Clue Chasers** - Enthusiastic Clue Scroll community\n\n**Ba Teams** - Barbarian Assault Games\n\n**Conquest Chat** - Conquest Games\n\n**Hybrid Top** - Fight Pit Games and finding partners to get a 'Hybrid Top' (Battle-mage/Trickster/Vanguard)\n\n**FishFlingers** - Fish Flingers\n\n**GOP Addict** - Great Orb Project Games\n\n**Heist4Fun** - Heist Games\n\n**World60pengs** or **W60 Find Pen** - Penguin Hunting\n\n**Fast SC** - Stealing Creation (Non-Combat)",
-    "thumbnail": {
-      "url": "https://cdn.discordapp.com/attachments/417729280135659526/418816703082790922/Minigames.png"
-    },
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
       "text": "Achievement Help | Friend Chats"
@@ -86,9 +80,6 @@ exports.data = {
     "skilling/d&ds": {
     "title": "Skilling + D&D's",
     "description":"**PoF Trade FC** - Player Owned Farm trading hub\n\n**CoreHunting** - Hall of Memories divination core calls\n\n**WC Idols** - Overgrown Idol location calls\n\n**Boxes** - Portable Deposit Box locations (On DXP)\n\n**Harmed Ore** - Harmonized Ore locations\n\n**NemiForest** - 9/9 Nemi Forests locations\n\n**Portables** - Portable skilling locations\n\n**Tree Hunt** - Crystal Tree locations\n\n**Star Find** - Shooting Stars locations\n\n**Fast Effigy** - Ancient Effigies help\n\n**whirlpooldnd** or **whirlpoolv2** - Deep Sea Fishing random event calls\n\n**DemonMobs FC** - Demon Flash Mobs locations\n\n**Evil Tree FC**, **Chase Trees**, or **Tree Hunters** - Evil Tree locations\n\n**Soulobby** -  Soul Obelisk Locations (Menaphos)\n\n** Free Leech** - Free leeching of Dungeoneering floors\n\n**Altar** - Finding Gilded Altar Hosts\n\n**DgRevolution** - Finding Dungeoneering partners\n\n**vis wax fc** - Vis Wax Combinations\n\n**pk** - No PK Warbands, Wildywyrms, Bossing. Apply [here](http://services.runescape.com/m=forum/forums.ws?45,46,850,65995412).\n\n**Masterbands** -  Loot and PK during Wilderness Warbands. Apply [here](http://services.runescape.com/m=forum/forums.ws?75,76,820,65957764).",
-    "thumbnail": {
-      "url": "https://media.discordapp.net/attachments/417729280135659526/418816915369099285/Skilling_DDs.png"
-    },
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
       "text": "Achievement Help | Friend Chats"
@@ -113,9 +104,6 @@ exports.data = {
     "pvm/teaching": {
     "title": "PvM/Teaching",
     "description":"**EliteDungFC** - finding teams for Elite Dungeons\n\n**Slayer Dungs** - Finding specific Player Owned Slayer Dungeon Mobs\n\n**AoDTeams** - Teams for Angel of Death\n\n**NexAod** - Angel of Death Masses\n\n**Raiding FC**, **Raid FC**, or **Aus Raiding** - Full Raids and learning\n\n**Boss School**, **PvMing FC**, **PvM Fanatics**, or **Teaching RS** - Teaching and finding any boss teams\n\n**lskk247** - Lootshare Kalphite King kills\n\n**Casual KK** - smaller experienced Kalphite King teams\n\n**Duck Hunters** - Megaduck Pet chances\n\n**Teachingrots** or **Pro Rots** -  Learning and making teams for Rise of the Six\n\n**Rago PvM** or **Wandoodle** -  Vorago Masses and Learning\n\nLSPVM247 - Boss LootSharing\n\n**Solak247** or **Solak FC** - Solak 2 or 7 man teams",
-    "thumbnail": {
-      "url": "https://media.discordapp.net/attachments/417729280135659526/418817069539131392/PvM_Teaching.png"
-    },
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
       "text": "Achievement Help | Friend Chats"
@@ -140,9 +128,6 @@ exports.data = {
     "price checking": {
     "title": "Price Checking/Trading",
     "description":"**90weaps pc** - Price checking T90+ Weapons\n\n**R Quark** - Price checking Rares\n\n**TH Rares PC** - Treasure Hunter/SOF price checking\n\n**Using the Forums for prices** - Incase there are issues with the friend chats use [this](http://services.runescape.com/m=forum/forums.ws?17,18,thd,372,65986200) official RuneScape Forums resource.",
-    "thumbnail": {
-      "url": "https://media.discordapp.net/attachments/417729280135659526/418817180361293824/Price_checking.png"
-    },
     "footer": {
       "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
       "text": "Achievement Help | Friend Chats"
