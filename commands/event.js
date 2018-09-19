de28a22018e977d555982527e705ae17ecbbbd7c
Maximillion <maximillionrh@gmail.com>
2018-09-20 00:42:59 +0100

    re-order checks and re-assign variables

--- commands/event.js
+++ commands/event.js
@@ -1,12 +1,12 @@
 exports.run = (client, message, args, level) => {
 
+	const {data} = require('../info/eventembeds.js');
 	if (!args[0]) return message.channel.send("Please specify a valid event name.");
-	const [type, time, date] = message.content.split("  ");
+	const [command, type, time, date] = message.content.split("  ");
+	const search = type.toLowerCase();
+	if (!Object.getOwnPropertyNames(data).includes(search)) return message.channel.send(`No guide embed exists for **${type}**. Please try again.`);
 	if (!time) return message.channel.send("Please specify a valid time.");
 	if (!date) return message.channel.send("Please specify a valid date.");
-	const search = type.toLowerCase();
-	const data = require('../info/eventembeds.js');
-	if (!Object.getOwnPropertyNames(data).includes(search)) return message.channel.send(`No guide embed exists for **${type}**. Please try again.`);
 	const embed = data[search];
 	embed.color = 2136831;
 	embed.timestamp = new Date();
