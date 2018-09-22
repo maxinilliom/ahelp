7646a21bed9e2d680b39158d2597c1a20e5d9fb5
Maximillion <maximillionrh@gmail.com>
2018-09-22 16:07:08 +0100

    add query embed

--- guides/reaperGuide.js
+++ guides/reaperGuide.js
@@ -23,6 +23,35 @@ exports.data = {
             "text": "Achievement Help"
         }
     },
+    "query": {
+    "title": "Reaper Command Query",
+    "author": {
+        "name": "AHelp How-To Info",
+        "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+    "description": "To find a specific achievement or guide you are looking for, you have two options.",
+    "thumbnail": {
+        "url": "https://2007rshelp.com/img/genimg/god/zaros.png"
+    },
+    "fields": [
+        {
+            "name": "Searching:",
+            "value": "The first option is to use the basic search command built into Discord. Simply search for the full or partial name of the requirement or achievement inside of the channel the guide is located in.\n\nFor example, if players want to find the Rewind guide for the Yakamaru Raids Feat, they can click on the search bar in the top right of Discord and type \"**in: rs-combat rewind**\". [Example](https://cdn.discordapp.com/attachments/398504241305747466/490200715625758749/unknown.png)."
+        },
+        {
+            "name": "Command Usage:",
+            "value": "The second option is to utilize our Discord bot, <@408471193675497473>, to spawn the information in the <#382701090430386180> channel. To do this, you must utilize the prefix, which in this case is a period, followed by the guide section you are looking in and the achievement or guide you are looking for.\n\n• .reaper help - Provides a list of all available commands to use for the RuneScape bosses.\n\n• .reaper <boss name> - Provides the guide for the given boss.\n\nNote that <> is not needed in any part of the command. All guides can be found by giving a shortened version of that name. If the command has no other identical command counterparts, the guide will directly spawn. If it does, a list of options for you to choose from with its corresponding number will spawn. [Example](https://gyazo.com/31aa9f30ff4f0fc571899c3604e3b13f)."
+        },
+        {
+            "name": "Tips:",
+            "value": "• You can collapse guides you do not wish to see by left-clicking on the channel category name. [Example](https://cdn.discordapp.com/attachments/398504241305747466/490198657111425042/unknown.png).\n\n• You can also mute channels by right-clicking them and selecting mute. All the guide channels are read-only, but we will be reposting them to amend any information or add new information as updates come. Muting every channel within a category will automatically hide the category from your view after the next time your Discord refreshes.\n\n• You can track the progress of all your achievements [here](https://docs.google.com/spreadsheets/d/19sVmY1BgnRkuLIJANjf42IEvh83u_mYqf6PY0E5KXuk/edit?usp=sharing). You will need to select **File** > **Make a copy..** in order to edit the sheet to your own achievement completion status."
+        }
+    ],
+    "footer": {
+        "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+        "text": "AHelp | Designed by Son and AHelpers"
+    }
+  },
     "araxxor pt1": {
                 "title": "Araxxor",
                 "author": {
