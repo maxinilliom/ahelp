9d01ece5dc06f9816cd5edee1644910d66f0f5f0
maxruahar <maximillionrh@gmail.com>
2018-09-24 18:57:45 +0000

    update default enabled commands

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,14 @@
-exports.run = (client, message, args, level) => {
+exports.run = async (client, message, args, level) => {
 
+	const content = message.content.substring(6);
+	const guild = message.guild;
+	const [type, time, date, ...details] = content.split("  ");
+	const [world, fc, voice] = details ? details : undefined;
+	const vc = voice ? guild.channels.find('name', voice) : undefined;
+	const inv = await vc.createInvite({maxAge: 3600}, `${type.toProperCase()} event.`);
 
+	message.channel.send(`${type.toProperCase()} at ${time} on ${date} in ${guild.name} on world ${world} in ${fc} FC using ${vc.name} for voice.`);
+	message.channel.send(`Join voice here: ${inv}`);
 
 };
 
