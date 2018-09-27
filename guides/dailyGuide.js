919dc18706633fb3f6e8fbe3a3a24ad708b66781
pedro aranda <pedro.aranda1879@gmail.com>
2018-09-26 19:18:45 -0700

    added new dailies

--- guides/dailyGuide.js
+++ guides/dailyGuide.js
@@ -97,6 +97,28 @@ exports.data = {
       }
     ]
   },
+  "coeden":{
+    "title": "Coeden Logs",
+    "description": "[Coeden](http://runescape.wikia.com/wiki/Coeden) is an [Ent](http://runescape.wikia.com/wiki/Ent) found in the [Crwys Clan](http://runescape.wikia.com/wiki/Crwys_Clan) district of [Prifddinas](http://runescape.wikia.com/wiki/Prifddinas), sitting on a rock inside a greenhouse that also contains some [choking ivy](http://runescape.wikia.com/wiki/Choking_ivy) for woodcutting. After completing the [Tirannwn Tasks](http://runescape.wikia.com/wiki/Tirannwn_Tasks), players can persuade Coeden each day to give a combination of 28 [elder](http://runescape.wikia.com/wiki/Elder_logs), [magic](http://runescape.wikia.com/wiki/Magic_logs) and [yew logs](http://runescape.wikia.com/wiki/Yew_logs).",
+    "url": "http://runescape.wikia.com/wiki/Coeden#Persuading",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/5/54/Coeden.png/revision/latest/scale-to-width-down/200?cb=20171101000636"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+     "fields": [
+      {
+        "name": "Useful Information",
+        "value": "Players must have an empty inventory to receive the logs. The chosen dialogue option for persuading Coeden doesn't affect the outcome of the logs. For a list of possible log combinations, click [here](http://runescape.wikia.com/wiki/Coeden#Daily_logs)."
+      }
+    ]
+  },
   "crystal flasks": {
     "title": "Crystal Flasks",
     "description": "[Crystal Flasks](http://runescape.wikia.com/wiki/Crystal_flask) require the completion of [Plague's End](http://runescape.wikia.com/wiki/Plague%27s_End). They can be made from [Crystal-flecked Sandstone](http://runescape.wikia.com/wiki/Crystal-flecked_sandstone).\n\n50 sandstone can be mined daily in the [Ithell District](http://runescape.wikia.com/wiki/Ithell_Clan), and an additional 25 in the [Edimmu Resource Dungeon](http://runescape.wikia.com/wiki/Edimmu_resource_dungeon), requiring 115 Dungeoneering. To operate the machine, a player must have completed the [As a First Resort](http://runescape.wikia.com/wiki/As_a_First_Resort) quest.",
@@ -139,6 +161,28 @@ exports.data = {
       {
         "name": "Useful Information:",
         "value": "Feathers will always be in demand, as they are used for Fletching and Fishing. When it comes to the feather packs, the full stock does not replenish each day. Instead, each store's stock of feather packs replenishes at a rate of about 1 per 12 hours of real time, independently. Keep in mind that [Gerrant's Fishy Business](http://runescape.wikia.com/wiki/Gerrant%27s_Fishy_Business) and [Lovecraft's Tackle](http://runescape.wikia.com/wiki/Lovecraft%27s_Tackle) share the same stock."
+      }
+    ]
+  },
+  "feathers of ma'at": {
+    "title": "Feathers of Ma'at",
+    "description": "Every day, 1000 [feathers of Ma'at](http://runescape.wikia.com/wiki/Feather_of_Ma%27at) can be bought for 1500 coins each from the [Menaphite guard](http://runescape.wikia.com/wiki/Menaphite_guard_(Sophanem_Slayer_Dungeon) outside the [Sophanem Slayer Dungeon](http://runescape.wikia.com/wiki/Sophanem_Slayer_Dungeon). The player will have needed to complete [Icthlarin's Little Helper](http://runescape.wikia.com/wiki/Icthlarin%27s_Little_Helper) in order to use this method.",
+    "url": "http://runescape.wikia.com/wiki/Money_making_guide/Buying_feathers_of_Ma%27at",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/4/4d/Feather_of_Ma%27at_detail.png/revision/latest?cb=20170605225654"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+     "fields": [
+      {
+        "name": "Useful Information",
+        "value": "The fastest way to reach the guard is by using a [Sophanem Slayer Dungeon teleport](http://runescape.wikia.com/wiki/Sophanem_Slayer_Dungeon_teleport) which teleports the player outside the dungeon and immediately next to the guard. If the player wishes not to use a teleportation tablet, the guard can be reached by teleporting to the [lodestone](http://runescape.wikia.com/wiki/Lodestone_Network) in [Menaphos](http://runescape.wikia.com/wiki/Menaphos) and crossing the bridge to [Sophanem](http://runescape.wikia.com/wiki/Sophanem). This requires completion if [Jack of Spades](http://runescape.wikia.com/wiki/Jack_of_Spades) and [Contact!](http://runescape.wikia.com/wiki/Contact!)"
       }
     ]
   },
@@ -205,6 +249,50 @@ exports.data = {
       {
         "name": "Useful Information:",
         "value": "The command **!vis** in <#382701090430386180> can be used to find out the days combinations. To make the most out of your coins, it's recommended to use the runes purchased from a [Rune Shop](http://runescape.wikia.com/wiki/Magic_shops) run.\n\nIt is also recommended to stick to elemental runes, as they tend to generate the most profit. A full list of runes and their profit potential can be found [here](http://runescape.wikia.com/wiki/Rune_Goldberg_Machine)."
+      }
+    ]
+  },
+  "weird old man": {
+  "title": "Weird Old Man",
+    "description": "After completing either the Medium, Hard, or Elite [Desert Tasks](http://runescape.wikia.com/wiki/Desert_Tasks), the [Weird Old Man](http://runescape.wikia.com/wiki/Weird_Old_Man) can be talked to once a day to claim 20 (medium), 30 (hard), or 40 (elite) noted [potato cactus](http://runescape.wikia.com/wiki/Potato_cactus).",
+    "url": "http://runescape.wikia.com/wiki/Money_making_guide/Claiming_potato_cacti_from_the_Weird_Old_Man",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/9/9e/Weird_Old_Man_chathead.png/revision/latest?cb=20130324000245"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+     "fields": [
+      {
+        "name": "Useful Information",
+        "value": "The Old Man is found just outside the [Kalphite Hive](http://runescape.wikia.com/wiki/Kalphite_Hive), and the fastest way to reach him is using the [fairy ring](http://runescape.wikia.com/wiki/Fairy_ring) code **b•i•q**. This teleport would require the completion of [Fairy Tale II - Cure a Queen](http://runescape.wikia.com/wiki/Fairy_Tale_II_-_Cure_a_Queen)."
+      }
+    ]
+  },
+  "wizard cromperty": {
+ "title": "Wizard Cromperty",
+    "description": "After completing the hard [Ardougne Tasks](http://runescape.wikia.com/wiki/Ardougne_Tasks), [Wizard Cromperty](http://runescape.wikia.com/wiki/Wizard_Cromperty) will give you 150 [pure essence](http://runescape.wikia.com/wiki/Pure_essence) every day, resetting at 00:00 UTC.",
+    "url": "http://runescape.wikia.com/wiki/Money_making_guide/Claiming_pure_essence_from_Wizard_Cromperty",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Helping you reach your goals, whatever they may be!"
+    },
+    "thumbnail": {
+      "url": "https://vignette.wikia.nocookie.net/runescape2/images/1/11/Wizard_Cromperty_chathead.png/revision/latest?cb=20120106142213"
+    },
+    "author": {
+      "name": "Max Cape Info",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+     "fields": [
+      {
+        "name": "Useful Information",
+        "value": "Wizard Cromperty is located in the building east of the [Servants' Guild](http://runescape.wikia.com/wiki/Servants%27_Guild). Right-click him to claim the essence. You don't need to be wearing your [ardougne cloak](http://runescape.wikia.com/wiki/Ardougne_cloak) to claim the essence. If you've only completed the medium tasks, you can still claim 100 essence per day."
       }
     ]
   },
