a9b77c7fd2cd5356fc4a2dc2cdaaef029894575f
Maximillion <maximillionrh@gmail.com>
2018-10-17 12:22:15 +0100

    commit misc guides

---
+++ info/misc.js
@@ -0,0 +1,327 @@
+exports.data = {
+  "break1": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+    "achievements": {
+    "title": "Achievements",
+    "description":"The following guides are meant to be used for more information regarding things that involve Achievements.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "[Zemomark Guide](https://www.youtube.com/watch?v=5Fu8KjGEM0I)\nBy: Drunkpriest666"
+      },
+      {
+        "name": "​",
+        "value": "[Calculator for Best Ticket/Thaler Combination](https://docs.google.com/spreadsheets/d/1vjrV5CxoC6sJ_3L_mGARkBKMhfhPsr5nQr7lpecHhHI/edit#gid=0)\nBy: Boat02"
+      },
+      {
+        "name": "​",
+        "value": "[Compiled List of Achievements](http://runescape.wikia.com/wiki/List_of_achievements)\nBy: <@220616902857195521> and the Wikian Team"
+      },
+      {
+        "name": "​",
+        "value": "[Level 3 to Master Quest Cape & Trimmed Completionist Cape](https://docs.google.com/spreadsheets/d/1uLxm0jvmL1_FJNYUJp6YqIezzqrZdjPf2xQGOWYd6ao/edit?usp=sharing)\nBy: <@111813700352016384>"
+      },
+      {
+        "name": "​",
+        "value": "[Time Locked Cape Requirements](https://www.reddit.com/r/runescape/comments/7mfcr0/psa_you_should_be_doing_these_reqs_if_going_for/)\nBy: <@212672527405678592>"
+      },            
+      {
+        "name": "​",
+        "value": "[Guide to Everything Ports](http://services.runescape.com/m=forum/forums.ws?75,76,519,65829739)\nBy: Portmaster Kags"
+      },
+      {
+        "name": "​",
+        "value": "[True Trim Requirement List](http://services.runescape.com/m=forum/forums.ws?48,49,834,65820293,30,339213816#30)\nBy: <@140239473739825153>"
+      }
+      ]
+  },
+  "break2": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+  "combat": {
+    "title": "Combat",
+    "description":"The following guides are meant to be used for more information regarding things that involve Combat in any way.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "[Invention Perk Guide](https://www.reddit.com/r/runescape/comments/7i6kae/invention_links_and_guides_for_all_the_plebs_lets/?st=JAX61KXQ&sh=cd2bfda8)\nBy: DeadLawRS3"
+      },
+      {
+        "name": "​",
+        "value": "**Ability Bar Guides**\n\n\t- [DPS for Dummies: Melee DPS Basics](https://www.youtube.com/watch?v=LIrXHNi00k8)\n\n\t- [DPS for Dummies: Magic DPS Basics](https://www.youtube.com/watch?v=afqRRgneJCw)\n\n\t- [DPS for Dummies: Ranged DPS Basics](https://www.youtube.com/watch?v=y5WK0oh6H0g)\nBy: The Junesong"
+      },
+      {
+        "name": "​",
+        "value": "[Morvran task sheet for the average adventurer](https://docs.google.com/spreadsheets/d/1SqjgJnZShBTzuodTTcmlaPhdGu9xHkXLHx-JkvskofI/htmlview#)\nBy: Charzila"
+      },
+      {
+        "name": "​",
+        "value": "[Slayer Tasks Calculations](https://docs.google.com/spreadsheets/d/1hYNMQ_2QjhebZJsMCXEGDarEekOT1uZiPGx0i26ILps/htmlview#)\nBy: Jona Than"
+      },
+      {
+        "name": "​",
+        "value": "[Player Owned Dungeons for Slayer Creature Needs](https://docs.google.com/spreadsheets/d/1NibABxK2xciVmIeuZ2gGEBZPe9NIoi7skprEiEuGBc0/edit#gid=516613908)\nBy: Roketrun"
+      },            
+      {
+        "name": "​",
+        "value": "[Efficient Slayer Dungeon Use (Level 99+)](https://www.reddit.com/r/runescape/comments/7j6lbr/guide_on_efficient_use_of_personal_slayer/?st=JB5T6PV0&sh=497c2871)\nBy: Kags"
+      },
+      {
+        "name": "​",
+        "value": "[Morvran Slayer Assignment Calculator](https://docs.google.com/spreadsheets/d/1TcyyaQnaoBv8OyX6vTqTwgIeM0cJcppVSV00WQkynmo/edit?usp=sharing)\nBy: <@241534285130104833>\n\n*“It's pretty simple to use. Everything is set to default weight values. Make the effective weights 0 if they are blocked, toggled off, or you havent unlocked the assignment. Double the effective weight if you prefered it.”*"
+      },
+      {
+        "name": "​",
+        "value": "[How to get into High Level PVM](https://www.reddit.com/r/runescape/comments/7uejtm/how_do_i_get_myself_into_highlevel_group_pvm_a/?st=JD4EZ9TM&sh=4d437f33)\nBy: Jet_Black_Hair"
+      },
+      {
+        "name": "​",
+        "value": "[IFB Companion](https://jpcmarques.github.io/IFBCompanion/)\nBy: Kyrioth"
+      },
+      {
+        "name": "​",
+        "value": "[IFB Drop Chance Calculator Original](https://docs.google.com/spreadsheets/d/1rnzDhOfrrh4Htg19IKvjxhuxz3taLVGCxisxqvO54IQ/edit#gid=0)\nBy: <@124627910232375296>"
+      },
+      {
+        "name": "​",
+        "value": "[IFB Drop Chance Calculator Variant](https://docs.google.com/spreadsheets/d/1G_vsn8pQH8YHfL7HoB6pQfNVEMIXM7Q3oXUZ1SqNxy4/edit?usp=sharing)\nBy: <@200568154735247360>"
+      }           
+      ]
+  },
+  "break3": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+    "skilling/d&ds": {
+    "title": "Dailies/D&D's",
+    "description":"The following guides are meant to be used for more information regarding things that have to do with Dailies/D&D's.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "[Shop Runs](https://docs.google.com/spreadsheets/d/17eUeogMBOUyou8SqupSDFbPbrHwh2e1Cvb-pGNekEog/edit?usp=sharing)\nBy: <@97928972305707008>"
+      },
+      {
+        "name": "​",
+        "value": "[Runescape Repeatables](https://docs.google.com/document/d/1-Qvra1QeW0u03J1DCaHNgUnvWevomeZRj7XxLLZnRTA/mobilebasic#)\nBy: Forthorn"
+      },
+      {
+        "name": "​",
+        "value": "[Warbands and Goebiebands Schedule/Vis Wax Combination Tracker](http://warbandtracker.com/schedule.php)\nBy: Proselyte Ko and Team"
+      },
+      {
+        "name": "​",
+        "value": "[Deep Sea Fishing Merchant Stock Guide](https://www.reddit.com/r/runescape/comments/8i7znp/traveling_merchant_reasons_to_buynot_to_buy_each/)\nBy: Triforc4link"
+      },
+      {
+        "name": "​",
+        "value": "[PoF Merchant Tracker](https://calendar.google.com/calendar/embed?src=pofdiscordrs%40gmail.com)\nBy: <@176094756315594752>"
+      }            
+      ]
+  },
+  "break4": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+  "ironman": {
+    "title": "Ironman",
+    "description":"The following guides are meant to be used for more information regarding things that have to do with the Ironman Mode.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "[The Ironman Bible](https://docs.google.com/document/d/133YcyqMghgvlZovjiGfqBMX5L28pFSB8TdP7vcTzO0Y/edit)\nBy: Dreyri"
+      },
+      {
+        "name": "​",
+        "value": "[Ironman Guide](https://docs.google.com/spreadsheets/d/17_13tJf3gQoDTMj9hmSFTTM_TVG-4TySWF1pPXkTp1w/edit#gid=42920584)\nBy: Manlover"
+      },
+      {
+        "name": "​",
+        "value": "[Level 3 to Quest Cape: Full Guide/Questing Order](https://www.reddit.com/r/runescape/comments/4c25bc/level_3_to_quest_cape_full_guidequesting_order/)\nBy: <@114959199926091781>"
+      },
+      {
+        "name": "​",
+        "value": "[Hardcore Ironman Safe Death List](https://media.discordapp.net/attachments/275755292753526784/415374075230289921/unknown.png)\nBy: RS Wiki"
+      }        
+      ]
+  },
+  "break5": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+    "other": {
+    "title": "Other",
+    "description":"The following guides are meant to be used for more information regarding things that don't fit into specific categories.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "[Ancient Bones Farming Guide](https://www.reddit.com/r/runescape/comments/7hzoqm/how_to_farm_ancient_bones_efficiently_while/?st=JAVIFYNM&sh=b336cf70)\nBy: Portmaster Kags"
+      },
+      {
+        "name": "​",
+        "value": "[Forum Signature Guide](http://services.runescape.com/m=forum/c=t*jh7EDgQW8/forums.ws?55,56,75,64795809)\nBy: Iceberg"
+      },
+      {
+        "name": "​",
+        "value": "[Guide to everything by everyone](https://docs.google.com/spreadsheets/d/1j19rWfbapAzxriJ4UQpzz4kSDTmbDN1W4Tdo4Fmchds)\nBy: Kewda"
+      },
+      {
+        "name": "​",
+        "value": "[RS Pocketbook](https://drive.google.com/file/d/0B1BQkGxSlc2tQmt5Yl9LeUV4d1E/view)\nBy: <@113866531066216450> and Verax"
+      },
+      {
+        "name": "​",
+        "value": "[[Runescape 3] How to farm Easy Clue Scrolls FAST](https://www.youtube.com/watch?v=qeUnUMbVm5A&t=1s)\nBy: puretppc"
+      },            
+      {
+        "name": "​",
+        "value": "**Clue Presets**\n\n\t- [Master Clues](https://cdn.discordapp.com/attachments/426541438369792012/492140745789276160/unknown.png)\n\n\t- [Elite Clues](https://cdn.discordapp.com/attachments/426541438369792012/492140832875610113/unknown.png)\n\n\t- [Hard Clues](https://cdn.discordapp.com/attachments/426541438369792012/492141057769865227/unknown.png) (empty space at the bottom is for the hard casket)\nBy: <@177599043118039040>"
+      },
+      {
+        "name": "​",
+        "value": "**Item Street Prices**\n\n\t- [Discontinued Items](http://services.runescape.com/m=forum/forums.ws?17,18,589,66050038)\n\n\tBy Lyra\n\n\t- [High Level Weapons](http://services.runescape.com/m=forum/forums.ws?17,18,673,66031411)\n\n\tBy Mr Moneyhold\n\n\t- [Codex Items](http://services.runescape.com/m=forum/forums.ws?17,18,106,66034614)\n\n\tBy DeathHunterX\n\n\t- [Treasure Trail Items](http://services.runescape.com/m=forum/forums.ws?17,18,706,66007326)\n\n\tBy Dr Sid PhD\n\n\t- [HSR and Miscellaneous items](http://services.runescape.com/m=forum/forums.ws?17,18,322,65901606)\n\n\t By J T V"
+      }
+      ]
+  },
+  "break6": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+  "skilling": {
+    "title": "Skilling",
+    "description":"The following guides are meant to be used for more information regarding things that have to do with Skilling.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    },
+          "fields": [
+      {
+        "name": "​",
+        "value": "[Invention Exp Calculator](https://docs.google.com/spreadsheets/d/1aJx78OGKb4IlKpq0fG8syC6Z7XEcTbBa1RLdX3YFrjI/edit#gid=0)\nBy: <@275679535595520000>"
+      },
+      {
+        "name": "​",
+        "value": "[Invention Batch 2: Machines Profit Calculator](https://docs.google.com/spreadsheets/d/1B2CpQlVWeK2JNuPGvZpFitqvp6ihU3VMKGBmwVJgSC4/htmlview#gid=0)\nBy: Sapnupuas"
+      },
+      {
+        "name": "​",
+        "value": "[[Runescape 3] Complete Double XP Weekend Guide 2017: Basics, Strategies + Tips/Tricks!](https://www.youtube.com/watch?v=mGbyl9l-YU0)\nBy: puretppc"
+      },
+      {
+        "name": "​",
+        "value": "[[Herblore guide] Making Overloads (2.7M xp/h - 2700 Overloads/h)](https://www.youtube.com/watch?v=PaGnpb6liiM)\nBy: Hugo"
+      },
+      {
+        "name": "​",
+        "value": "[Dungeoneering Guide: how to improve floor times](https://docs.google.com/document/d/1Kluwf-R4wPAwRxC4vXcyIfrTVrzWaVzuu2BzJUXihYI/edit?usp=sharing)\nBy: <@215180067511336962>"
+      },
+      {
+        "name": "​",
+        "value": "[Item and Pet Drop Probability](https://runeapps.org/odds-skillpets)\nBy: Skillbert"
+      } 
+      ]
+  },
+    "break7": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  },
+    "information": {
+    "title": "Want to add a Guide?",
+    "description":"If you have a guide you wish to submit, please message an <@&456091063703044106>+ in rank and they will ensure it is added. You may also post the guide in the <#488776619125899264> channel.",
+    "thumbnail": {
+      "url": "https://images-ext-1.discordapp.net/external/PVZ6gFLOXwgF6SeLOoW5hNTaZTSqEqO21dtNAgrXVTg/%3Fcb%3D20140317195258/https/vignette.wikia.nocookie.net/runescape2/images/3/3d/RuneScape_3_Logo.png/revision/latest"
+    },
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/attachments/297388220231057419/400471386101121024/image.jpg",
+      "text": "Achievement Help | Connecting our Helpful Communities!"
+    },
+    "author": {
+      "name": "Miscellaneous Guides & Tips",
+      "icon_url": "https://i.imgur.com/6c6q2iC.png"
+    }
+  },
+  "break8": {
+    "files": [{
+      "attachment": "media/img/info/fc/break.png",
+      "name": "break.png"
+    }]
+  }
+};
