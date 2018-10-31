659fc904a24ff79317b812b36663685644f88aa0
Ethan Smith <ethanverdellsmith@gmail.com>
2018-10-31 12:27:41 -0400

    removed FC and amended the clan portion

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,7 +9,7 @@ module.exports = async (client, member) => {
 	const replyChannel = client.channels.get('435588740346413057');
 	const logChannel = client.channels.get('407919969712603145');
 	const rs = '• 1,882/2,046 achievements or 19,352/21,035 RuneScore';
-	const wMsg = `\n\nIf you have any of the following, please let us know and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss or Master of All titles\n${rs}\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with more than 50 followers\n\nYou can find us in our clan in game: '**Alright**' on World 68\nWe also have a friend chat in game: '**Saint Cannon**'\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
+	const wMsg = `\n\nIf you have any of the following, please let us know and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss or Master of All titles\n${rs}\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with more than 50 followers\n\nIf you need help or a community in game, you can guest our clan: '**Alright**' on World 68\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
 	const maxUsers = "5";
 
 /*	const collEnd = (coll, arr) => {
