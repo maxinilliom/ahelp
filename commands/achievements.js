e25669d2180f864197a2c7882545afe33d3e16d3
Maximillion <maximillionrh@gmail.com>
2018-11-06 13:13:54 +0000

    commit command to inform users of new achievements

---
+++ commands/achievements.js
@@ -0,0 +1,34 @@
+exports.run = async (client, message, args, level) => {
+
+	const embed = {
+    "embed": {
+      "title": "Comprehensive list of new achievements",
+      "description": "If you are looking to complete the 139 new achievements released in the Achievement Bonanza update and bump up your RuneScore, check out [this page]() on the RuneScape Wiki for a list of them all.\n\nAll thanks to <@>!",
+      "color": 2927667,
+      "footer": {
+        "icon_url": "https://vignette.wikia.nocookie.net/runescape2/images/7/77/RuneScore.png",
+        "text": message.guild.name
+      }
+    }
+  };
+
+  embed.timestamp = new Date();
+  message.channel.send(embed);
+  message.delete();
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ach", "achieve", "achievement", "bonanza"],
+  permLevel: "User",
+  guilds: []
+};
+
+exports.help = {
+  name: "achievements",
+  category: "Information",
+  description: "Link to the RS Wiki page for all achievements added with the Achievement Bonanza.",
+  usage: "achievements"
+};
