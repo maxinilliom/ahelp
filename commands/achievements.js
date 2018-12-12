7cd26cb8f5872e88cd2091667399c246b6b8426b
maxruahar <maximillionrh@gmail.com>
2018-12-12 00:00:22 +0000

    update achievement count

--- commands/achievements.js
+++ commands/achievements.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args, level) => {
         "name": "Comprehensive list of new achievements",
         "icon_url": "https://vignette.wikia.nocookie.net/runescape2/images/7/77/RuneScore.png"
       },
-      "description": "\nIf you are looking to complete the 139 new achievements released in the Achievement Bonanza update and bump up your RuneScore, check out [this page](https://runescape.wiki/w/User:Salix_of_Prifddinas/Achievement_Bonanza) on the RuneScape Wiki for a list of them all.\n\nAll thanks to <@220616902857195521>!",
+      "description": "\nIf you are looking to complete the 142 new achievements released in the Achievement Bonanza update and bump up your RuneScore, check out [this page](https://runescape.wiki/w/User:Salix_of_Prifddinas/Achievement_Bonanza) on the RuneScape Wiki for a list of them all.\n\nAll thanks to <@220616902857195521>!",
       "color": 2927667,
       "footer": {
         "icon_url": message.guild.iconURL,
