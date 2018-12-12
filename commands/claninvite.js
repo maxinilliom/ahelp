675038e475acc6319348b590c669a7e55a536f97
maxruahar <maximillionrh@gmail.com>
2018-12-12 00:00:09 +0000

    initial commit

---
+++ commands/claninvite.js
@@ -0,0 +1,21 @@
+exports.run = async (client, message, args, level) => {
+
+  message.channel.send("https://discord.gg/KwysmXr");
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["clan", "claninv"],
+  permLevel: "User",
+  guilds: [],
+  cooldown: 5000
+};
+
+exports.help = {
+  name: "claninvite",
+  category: "Info",
+  description: "Fetch an invite for the Alright clan server.",
+  usage: "claninvite"
+};
