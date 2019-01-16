da5f8a66311a18179b44c2fe0b9a326836152434
maxruahar <maximillionrh@gmail.com>
2019-01-16 02:08:42 +0000

    update solak fc info

--- commands/solak.js
+++ commands/solak.js
@@ -20,8 +20,12 @@ exports.run = async (client, message, args, level) => {
         "value": "As we cannot currently offer Solak leeches for Reaper kills, we have allied with Solak FC, who has partnered with us in an attempt to help our members where we are currently unable. The load of a free service is extensive, so expect a wait time. Our team will continue to practice as time allows to help share the load when we are able. We are happy to work with our partners to mutually benefit the community and bring support to their efforts. They do have some requirements, which are listed below.\n\n__Requirements:__\n- Watched a YouTube guide or read through the Discord guide\n- Willing to join voice chat for the kill\n- Tier 90+ perked weapons\n- Tier 80+ perked armour\n- Tier 95 prayers\n- Dark Magic/Mahjarrat/Invigorate/Vampyrism Aura\n- Godbook/Scrimshaw/Erethdor’s Grimoire\n- Dominion Mines\n\nYou can join their server here: https://discord.gg/stdyvsa"
       },
       {
+        "name": "​",
+        "value": "The Solak FC Admin team also sell paid Solak leeches for 100m for those that do not want to wait on their free leech list. They operate during EU hours.\n\nYou can find their discord here: https://discord.gg/U4GRQVX"
+      },
+      {
         "name": "__**Option 2**__",
-        "value": "If you do not meet the above requirements or would prefer to not wait in line, you may utilise EasyLeech's paid service. They have been verified as trustworthy by us and many users have left feedback for their kills.\n\nTo help our members, their price for players who come from our server is discounted from 120m to 108m for a Solak leech kill. They also offer other leech services which you can read about in their server. We hope that this will help members get their Solak Reaper kill for the Completionist achievement.\n\nIf you have any questions or feedback, please feel free to share it with us! You can join their server and learn more about it here: https://discord.gg/CHhmrz3"
+        "value": "If you do not meet the above requirements or EU hours do not work for you, you may utilise EasyLeech's paid service. They have been verified as trustworthy by us and many users have left feedback for their kills.\n\nTo help our members, their price for players who come from our server is discounted from 120m to 108m for a Solak leech kill. They also offer other leech services which you can read about in their server. We hope that this will help members get their Solak Reaper kill for the Completionist achievement.\n\nIf you have any questions or feedback, please feel free to share it with us! You can join their server and learn more about it here: https://discord.gg/CHhmrz3"
       }
       ]
   }
