6b387498a650429e22b6fb9451fb3b5a8c044de9
MadClikr <40070318+MadClikr@users.noreply.github.com>
2019-05-26 11:03:21 -0400

    Update solak.js
    Updated the Solak command.  Free leeches are no longer being offered by the Solak FC.

--- commands/solak.js
+++ commands/solak.js
@@ -16,12 +16,8 @@
     },
           "fields": [
       {
-        "name": "__**Solak FC**__",
-        "value": "As we cannot currently offer Solak leeches for Reaper kills, we have allied with Solak FC in an attempt to help our members where we are currently unable. The load of a free service is extensive, so expect a wait time. Our team will continue to practice as time allows to help share the load when we are able. We are happy to work with our partners to mutually benefit the community and bring support to their efforts. They do have some requirements, which are listed below.\n\n__Requirements:__\n- Watched a YouTube guide or read through the Discord guide\n- Willing to join voice chat for the kill\n- Tier 90+ perked weapons\n- Tier 80+ perked armour\n- Tier 95 prayers\n- Dark Magic/Mahjarrat/Invigorate/Vampyrism Aura\n- Godbook/Scrimshaw/Erethdor’s Grimoire\n- Dominion Mines\n\nYou can join their server here: https://discord.gg/stdyvsa"
-      },
-      {
         "name": "Cheap Solak/PvM Leeches",
-        "value": "The Solak FC Admin team have created another server to sell *paid* Solak leeches at a price of 90m per leech. This is intended for those that do not want to wait on their free leech list. This service operates mostly during EU hours.\n\nYou can find their discord here: https://discord.gg/U4GRQVX"
+        "value": "Unfortunately, we cannot currently offer Solak leeches for Reaper kills, so we have allied with the Solak FC which sells *paid* Solak leeches at a price of 90m per leech.  \n\nYou can find their discord here: https://discord.gg/U4GRQVX"
       }
       ]
   }
