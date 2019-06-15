9a7545b50529e25c7a7e42173a6c6721ba408c5a
maxruahar <maximillionrh@gmail.com>
2019-06-15 20:31:17 +0100

    ignore misc guilds

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,9 @@
 module.exports = (client, member) => {
 	const settings = client.settings.get(member.guild.id);
 	const logChannel = client.channels.get('407919969712603145');
+	const guild = member.guild;
+	if (guild.id !== "382696689812766720") return;
+	
 	const newUsers = client.newUsers;
 	if (newUsers.has(member.id)) {
 		newUsers.delete(member.id);
