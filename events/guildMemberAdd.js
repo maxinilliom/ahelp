196c6f836fd37733827270ab918607cec9af371b
maxruahar <maximillionrh@gmail.com>
2019-06-26 13:40:11 +0100

    update achievement master reqs

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,7 +8,7 @@ module.exports = async (client, member) => {
 	const welcomeChannel = client.channels.get("382700760317558786");
 	const replyChannel = client.channels.get('435588740346413057');
 	const logChannel = client.channels.get('407919969712603145');
-	const rs = '• 2,125/2,310 achievements or 22,448/24,400 RuneScore';
+	const rs = '• 2,123/2,308 achievements or 22,429/24,380 RuneScore';
 	const wMsg = `\n\nIf you have any of the following, please let us know and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss or Master of All titles\n${rs}\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with more than 50 followers\n\nIf you need help or a community in game, you are welcome to guest in our clan: **'Alright'** on World 68 and join our Friends Chat **'AHelp'**\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
 	const maxUsers = "5";
 
