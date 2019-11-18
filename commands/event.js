a6c146d72a8147b26c015ef02f733eb1c5d3cd6c
maxruahar <maximillionrh@gmail.com>
2019-11-18 10:46:56 +0000

    clarify and update command to include hosts

--- commands/event.js
+++ commands/event.js
@@ -1,58 +1,77 @@
 exports.run = async (client, message, args, level) => {
 
+  // Load event embeds
 	const {data} = require('../info/eventembeds.js');
+
+  // Populate list of embeds
 	const list = Object.getOwnPropertyNames(data);
+
+  // Ensure only title embeds are listed (no pt2, pt3, etc.)
 	const helpArr = [];
 	list.forEach(e => {
 		if (!/\bpt\d/.test(e)) helpArr.push(e);
 	});
+
+  // "Help" argument will list valid embeds to channel
 	if (args[0] == "help") {
 		return message.channel.send(helpArr);
 	}
-	let inv, direction;
+
+  // Sort data into information
 	const guild = message.guild;
 	const content = message.content.substring(7);
-	const [type, time, date, ...details] = content.split("  ");
+	const [type, time, date, host, ...details] = content.split("  ");
   const [world, fc, voice] = details ? details : undefined;
+
+  // Check for mandatory information and return if not submitted
 	if (!type) return message.channel.send("Please specify a valid event name.");
 	if (!time) return message.channel.send("Please specify a valid time.");
 	if (!date) return message.channel.send("Please specify a valid date.");
+  if (!host) return message.channel.send("Please specify the host(s) name(s).");
+
+  // Search event embeds for submitted embed name
 	const search = type.toLowerCase();
 	if (!list.includes(search)) return message.channel.send(`No guide embed exists for **${type}**. Please try again.`);
+
+  // Load embed and set colour
 	const embed = data[search];
 	embed.color = 2136831;
+
+  // Remove "When" field if already existing and place new "When" field at top of embed
 	if (embed.fields[0].name == "When:") embed.fields.splice(0, 1);
 	const when = {
 		"name": "When:",
 		"value": `${time} game time on ${date}.`
 	};
 	embed.fields.unshift(when);
-	if (world) embed.fields[1].value = embed.fields[1].value.replace("World 68", `World ${world}`);
-	if (fc) embed.fields[1].value = embed.fields[1].value.replace("<@113770763261181961>", `${fc}`);
-	if (voice) {
-		if (!client.channels.find('name', voice)) return message.channel.send("Please specify a valid voice channel name.")
-  	const vc = client.channels.find('name', voice);
-  	if (guild.id == "485523397179342848" && vc.id !== "486308546661842944"
-			|| guild.id == "382696689812766720" && vc.id !== "425508386713632771") {
-  		//AH event posted in Alright or Alright event posted in AH
-  		direction = ` Voice chat will take place in ${vc.guild.name}'s ${vc.name} voice channel.`
-  	}
-  	else {
-  		direction = ` Voice chat will take place in this server's ${vc.name} voice channel.`;
-  	}
-  	inv = await vc.createInvite({maxAge: 7200}, `${embed.author.name}.`);
-		embed.fields[1].value += direction;
-	}
+
+  // Remove "Who" field if already existing and place new "Who" field as third field of embed
+  if (embed.fields[2].name == "Who:") embed.fields.splice(2, 1);
+  const who = {
+    "name": "Who:",
+    "value": host
+  }
+  embed.fields.splice(2, 0, who)
+
+  // Replace default world and/or FC values if custom values submitted
+  const where = embed.fields[1].value;
+	if (world) embed.fields[1].value = embed.fields[1].value.replace("{{world}}", world.match(/\d{1,3}/)[0]);
+	if (fc) embed.fields[1].value = embed.fields[1].value.replace("{{fc}}", fc);
+
+  // Send embed to channel the command is being used in
 	await message.channel.send({embed: embed});
+
+  // If it's a multi-part event embed send the second part
 	if (data[`${search} pt2`]) {
 		const second = data[`${search} pt2`];
 		second.color = 2136831;
 		await message.channel.send({embed: second});
 	}
-	if (inv) message.channel.send(`Join voice here: ${inv}`);
-	embed.fields[1].value = embed.fields[1].value.replace(`${fc}`, "<@113770763261181961>");
-	embed.fields[1].value = embed.fields[1].value.replace(`World ${world}`, "World 68");
-	embed.fields[1].value = embed.fields[1].value.replace(direction, "");
+
+  // Reset embed values to default
+	embed.fields[1].value = where;
+
+  // Remove the user's invoking message
 	message.delete();
 
 };
@@ -69,5 +88,5 @@ exports.help = {
   name: "event",
   category: "Information",
   description: "Post an embed for an upcoming event.",
-  usage: "event <help/type>  <time>  <date>  [world]  [friends chat]  [voice channel]"
+  usage: "event <help/type>  <time>  <date>  <host(s)>  [world]  [friends chat]  [voice channel]"
 };
