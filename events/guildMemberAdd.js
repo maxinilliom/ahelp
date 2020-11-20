5f4bda0341107f87a4881dbbfc75e1c41099c06c
Maximillion <maximillionrh@gmail.com>
2020-11-20 19:20:40 +0000

    Populate requirement from amReq enmap

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,39 +2,40 @@ const Discord = require("discord.js");
 const { inspect } = require("util");
 module.exports = async (client, member) => {
 
-	const guild = member.guild;
-	if (guild.id !== "382696689812766720") return;
-	const settings = client.settings.get(guild.id);
-	const welcomeChannel = client.channels.get("382700760317558786");
-	const replyChannel = client.channels.get('435588740346413057');
-	const logChannel = client.channels.get('407919969712603145');
-	const rs = '• 2,702/2,938 achievements or 26,445/28,745 RuneScore';
-	const wMsg = `\n\nIf you have any of the following, please let us know here and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss, Ultimate Slayer, or Master of All titles\n${rs}\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with 100 or more followers\n\nIf you need help or a community in game, you are welcome to guest in our clan: **'Alright'** on World 68 and join our Friends Chat **'AHelp'**\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
-	const maxUsers = "5";
+  const guild = member.guild;
+  if (guild.id !== "382696689812766720") return;
+  const settings = client.settings.get(guild.id);
+  const welcomeChannel = client.channels.get("382700760317558786");
+  const replyChannel = client.channels.get("435588740346413057");
+  const logChannel = client.channels.get("407919969712603145");
+  const rs = "• 2,702/2,938 achievements or 26,445/28,745 RuneScore";
+  const amReq = client.amReq;
+  const wMsg = `\n\nIf you have any of the following, please let us know here and we will assign the correct roles to signify:\n• Max | Comp | MQC | Comp (t) Cape\n• Insane Final Boss, Ultimate Slayer, or Master of All titles\n• ${amReq}\n• Max Total Virtual Level or Max Experience\n• A HCIM or IM Account you actively play on\n• You are a RS Twitch streamer or YouTuber with 100 or more followers\n\nIf you need help or a community in game, you are welcome to guest in our clan: **'Alright'** on World 68 and join our Friends Chat **'AHelp'**\n\nThanks for joining us! (:\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`;
+  const maxUsers = "5";
 
-/*	const collEnd = (coll, arr) => {
+  /*	const collEnd = (coll, arr) => {
 		coll.stop();
 	}
 */
-	const welcome = async () => {
-		if (client.newUsers.size == "0") return;
-		let userIDs = client.newUsers.map(u => u.id);
-		let userArr = client.newUsers.map(u => u.toString());
+  const welcome = async () => {
+    if (client.newUsers.size == "0") return;
+    let userIDs = client.newUsers.map(u => u.id);
+    let userArr = client.newUsers.map(u => u.toString());
 
-		client.newUsers.clear();
-		let userArrOld = userArr;
-		let userIDsOld = userIDs;
-		userArr = "";
-		userIDs = [];
+    client.newUsers.clear();
+    const userArrOld = userArr;
+    const userIDsOld = userIDs;
+    userArr = "";
+    userIDs = [];
 
-		if (userArrOld.length > 1) {
-			const last = `and ${userArrOld.pop()}`;
-			userArrOld.push(last);
-		}
-		let userList = userArrOld.join(", ");
-		const msg = `Welcome in ${userList}!${wMsg}`;
-		welcomeChannel.send(msg);
-/*		let output = [];
+    if (userArrOld.length > 1) {
+      const last = `and ${userArrOld.pop()}`;
+      userArrOld.push(last);
+    }
+    const userList = userArrOld.join(", ");
+    const msg = `Welcome in ${userList}!${wMsg}`;
+    welcomeChannel.send(msg);
+    /*		let output = [];
 		const msgs = [];
 
 		client.msgColl = new Discord.MessageCollector(welcomeChannel, m => userIDsOld.includes(m.author.id), {maxMatches: userArrOld.length});
@@ -49,12 +50,12 @@ module.exports = async (client, member) => {
 			replyChannel.send(`${output.join("\n")}\nAll users' responses logged or timed out for ${userList}`);
 		});
 */
-	}
+  };
 
-	if (client.welcomeTimer._called !== false) client.welcomeTimer = setTimeout(welcome, 5 * 60 * 1000);
+  if (client.welcomeTimer._called !== false) client.welcomeTimer = setTimeout(welcome, 5 * 60 * 1000);
 
-	client.newUsers.set(member.id, member.user);
+  client.newUsers.set(member.id, member.user);
 
-	if (client.newUsers.size == maxUsers) welcome();
+  if (client.newUsers.size == maxUsers) welcome();
 
-}
+};
